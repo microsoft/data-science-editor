134725f703cbd3a6923b7272bbae014a35a10731
pelikhan <jhalleux@microsoft.com>
2021-03-10 14:37:23 +0100

    more updates

---
+++ src/pages/tools/led-animation-designer.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import LedAnimationDesigner from "../../components/tools/LedAnimationDesigner"
+
+export default function Page() {
+    return <>
+        <h1>LED Animation Designer</h1>
+        <LedAnimationDesigner />
+    </>
+}
