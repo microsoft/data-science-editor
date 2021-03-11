eec6ef63f51627ba40a1bcd494cd3ace0f717afb
peli <pelikhan@users.noreply.github.com>
2021-03-11 02:45:42 -0800

    added link to help

--- src/pages/tools/service-test-editor.tsx
+++ src/pages/tools/service-test-editor.tsx
@@ -1,6 +1,19 @@
+import { Link } from "gatsby-theme-material-ui"
 import React from "react"
-import ServiceTestEditor from '../../components/tools/ServiceTestEditor'
+import ServiceTestEditor from "../../components/tools/ServiceTestEditor"
 
 export default function Page() {
-    return <ServiceTestEditor />
+    return (
+        <>
+            <h1>Service Test Editor</h1>
+            <p>
+                Read the{" "}
+                <Link to="/reference/service-tests">
+                    service test
+                </Link>{" "}
+                documentation.
+            </p>
+            <ServiceTestEditor />
+        </>
+    )
 }
