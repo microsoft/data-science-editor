25c25ae5051a0969d630c334b97a75c26abb6448
pelikhan <jhalleux@microsoft.com>
2021-03-12 09:17:24 +0100

    update role handling

--- src/components/services/useServiceRole.ts
+++ src/components/services/useServiceRole.ts
@@ -1,21 +1,11 @@
 import { useEffect, useState } from "react"
-import { CHANGE, SRV_ROLE_MANAGER } from "../../../jacdac-ts/src/jdom/constants"
-import { RoleManagerClient, RequestedRole } from "../../../jacdac-ts/src/jdom/rolemanagerclient"
+import { ROLE_CHANGE } from "../../../jacdac-ts/src/jdom/constants"
 import { JDService } from "../../../jacdac-ts/src/jdom/service"
-import useServices from "../hooks/useServices"
-import useServiceClient from "../useServiceClient"
 
 export default function useServiceRole(service: JDService) {
-    const [roleManager] = useServices({ serviceClass: SRV_ROLE_MANAGER })
-    const roleManagerClient = useServiceClient(
-        roleManager,
-        srv => new RoleManagerClient(srv)
-    );
-    const [role, setRole] = useState<RequestedRole>(roleManagerClient?.role(service));
-    useEffect(() => roleManagerClient?.subscribe(CHANGE, () => {
-        const r = roleManagerClient?.role(service);
-        setRole(r);
-    }), [roleManagerClient])
-
+    const [role, setRole] = useState<string>(service?.role);
+    useEffect(() => service?.subscribe(ROLE_CHANGE, () => {
+        setRole(service.role)
+    }), [service])
     return role;
 }
