2259b5e5afbd6fd4b938943b4426b3702d6da572
pelikhan <jhalleux@microsoft.com>
2021-03-15 09:29:36 +0100

    updated device icons

--- src/components/icons/TransportIcon.tsx
+++ src/components/icons/TransportIcon.tsx
@@ -7,16 +7,27 @@ import {
     BLUETOOTH_TRANSPORT,
     PACKETIO_TRANSPORT,
     USB_TRANSPORT,
+    VIRTUAL_DEVICE_NODE_NAME,
 } from "../../../jacdac-ts/src/jdom/constants"
+import KindIcon from "../KindIcon"
 
-export default function TransportIcon(props: { type: string }) {
-    const { type } = props
-
-    return (
-        <>
-            {type === USB_TRANSPORT && <UsbIcon />}
-            {type === BLUETOOTH_TRANSPORT && <BluetoothIcon />}
-            {type === PACKETIO_TRANSPORT && <UsbIcon />}
-        </>
-    )
+export default function TransportIcon(props: {
+    type: string
+    className?: string
+}) {
+    const { type, className } = props
+    switch (type) {
+        case PACKETIO_TRANSPORT:
+        case USB_TRANSPORT:
+            return <UsbIcon className={className} />
+        case BLUETOOTH_TRANSPORT:
+            return <BluetoothIcon className={className} />
+        default:
+            return (
+                <KindIcon
+                    kind={VIRTUAL_DEVICE_NODE_NAME}
+                    className={className}
+                />
+            )
+    }
 }
