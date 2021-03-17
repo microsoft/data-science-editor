d8437e00015a45a0441574352e8e6bc88d4c3276
pelikhan <jhalleux@microsoft.com>
2021-03-17 23:31:46 +0100

    adding temperature icon

--- src/components/hooks/useUnitIcon.tsx
+++ src/components/hooks/useUnitIcon.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React from "react"
 import HumidityIcon from "../icons/HumidityIcon"
+import TemperatureIcon from "../icons/TemperatureIcon"
 import { resolveUnit } from "../../../jacdac-ts/jacdac-spec/spectool/jdspec"
-import { SvgIconProps } from "@material-ui/core";
+import { SvgIconProps } from "@material-ui/core"
 
 export default (unit: string, props?: SvgIconProps) => {
-    const { unit: runit } = resolveUnit(unit) || {};
+    const { unit: runit } = resolveUnit(unit) || {}
     switch (runit) {
-        case "%RH": return <HumidityIcon {...props} />;
-        default: return null;
+        case "%RH":
+            return <HumidityIcon {...props} />
+        case "°C":
+        case "K":
+            return <TemperatureIcon {...props} />
+        default:
+            return null
     }
 }
