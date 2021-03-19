deb055952a2964cc5d3700a1ab491dc3e22b4c3f
pelikhan <jhalleux@microsoft.com>
2021-03-19 08:32:49 +0100

    clean out led animations

--- src/components/useMdxComponents.tsx
+++ src/components/useMdxComponents.tsx
@@ -1,38 +1,80 @@
-import React, { lazy, useMemo } from "react";
-import { Link } from "gatsby-theme-material-ui";
-import { Box, Paper, Table, TableBody, TableContainer, TableHead, TableRow, useTheme } from "@material-ui/core";
+import React, { lazy, useMemo } from "react"
+import { Link } from "gatsby-theme-material-ui"
+import {
+    Box,
+    Paper,
+    Table,
+    TableBody,
+    TableContainer,
+    TableHead,
+    TableRow,
+    useTheme,
+} from "@material-ui/core"
 
 import Suspense from "./ui/Suspense"
-const CodeBlock = lazy(() => import('./CodeBlock'));
+const CodeBlock = lazy(() => import("./CodeBlock"))
 const RandomGenerator = lazy(() => import("./RandomGenerator"))
-const TraceList = lazy(() => import("./TraceList"));
-const SpecificationUnitList = lazy(() => import("./SpecificationUnitList"));
-const StatusLEDAnimation = lazy(() => import("./StatusLEDAnimation"));
+const TraceList = lazy(() => import("./TraceList"))
+const SpecificationUnitList = lazy(() => import("./SpecificationUnitList"))
 
 export default function useMdxComponents() {
-  const theme = useTheme();
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const mdxComponents: any = useMemo(() => ({
-    Link: props => <Link color="textPrimary" {...props} />,
-    a: (props: { href: string }) => <Link color="textPrimary" {...props} rel="noopener noreferrer" />,
-    pre: props => <Box mb={theme.spacing(0.5)}><Paper>
-      <div {...props} />
-    </Paper></Box>,
-    table: props => <Box mb={theme.spacing(0.5)}><TableContainer component={Paper}>
-      <Box m={theme.spacing(0.5)}>
-        <Table size="small" {...props} />
-      </Box>
-    </TableContainer></Box>,
-    thead: props => <TableHead {...props} />,
-    tbody: props => <TableBody {...props} />,
-    tr: props => <TableRow {...props} />,
+    const theme = useTheme()
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const mdxComponents: any = useMemo(
+        () => ({
+            Link: props => <Link color="textPrimary" {...props} />,
+            a: (props: { href: string }) => (
+                <Link
+                    color="textPrimary"
+                    {...props}
+                    rel="noopener noreferrer"
+                />
+            ),
+            pre: props => (
+                <Box mb={theme.spacing(0.5)}>
+                    <Paper>
+                        <div {...props} />
+                    </Paper>
+                </Box>
+            ),
+            table: props => (
+                <Box mb={theme.spacing(0.5)}>
+                    <TableContainer component={Paper}>
+                        <Box m={theme.spacing(0.5)}>
+                            <Table size="small" {...props} />
+                        </Box>
+                    </TableContainer>
+                </Box>
+            ),
+            thead: props => <TableHead {...props} />,
+            tbody: props => <TableBody {...props} />,
+            tr: props => <TableRow {...props} />,
 
-    code: props => <Suspense><CodeBlock {...props} /></Suspense>,
-    RandomGenerator: props => <Suspense><Box displayPrint="none"><RandomGenerator {...props} /></Box></Suspense>,
-    TraceList: props => <Suspense><TraceList {...props} /></Suspense>,
-    SpecificationUnitList: props => <Suspense><SpecificationUnitList {...props} /></Suspense>,
-    StatusLEDAnimation: props => <Suspense><StatusLEDAnimation {...props} /></Suspense>,
-  }), []);
+            code: props => (
+                <Suspense>
+                    <CodeBlock {...props} />
+                </Suspense>
+            ),
+            RandomGenerator: props => (
+                <Suspense>
+                    <Box displayPrint="none">
+                        <RandomGenerator {...props} />
+                    </Box>
+                </Suspense>
+            ),
+            TraceList: props => (
+                <Suspense>
+                    <TraceList {...props} />
+                </Suspense>
+            ),
+            SpecificationUnitList: props => (
+                <Suspense>
+                    <SpecificationUnitList {...props} />
+                </Suspense>
+            ),
+        }),
+        []
+    )
 
-  return mdxComponents;
+    return mdxComponents
 }
--- src/pages/tools/led-animation-designer.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import LedAnimationDesigner from "../../components/tools/LedAnimationDesigner"
-
-export default function Page() {
-    return <>
-        <h1>LED Animation Designer</h1>
-        <LedAnimationDesigner />
-    </>
-}
