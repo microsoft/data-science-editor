0b62107d3f9bc5bf5d07f12537a8ad373d5370cc
peli <pelikhan@users.noreply.github.com>
2021-03-22 08:45:57 -0700

    cleanup settings manager

--- src/components/tools/SettingsManager.tsx
+++ src/components/tools/SettingsManager.tsx
@@ -1,28 +1,16 @@
-import React, { useContext } from "react"
+import React from "react"
 import { Grid } from "@material-ui/core"
-import JacdacContext, { JacdacContextProps } from "../../jacdac/Context"
 import { SRV_SETTINGS } from "../../../jacdac-ts/src/jdom/constants"
-import useChange from "../../jacdac/useChange"
 import ConnectAlert from "../alert/ConnectAlert"
-import Alert from "../ui/Alert"
 import SettingsCard from "../SettingsCard"
+import useServices from "../hooks/useServices"
 
 export default function SettingsManager() {
-    const { bus } = useContext<JacdacContextProps>(JacdacContext)
-
-    const services = useChange(bus, b =>
-        b.services({ serviceClass: SRV_SETTINGS })
-    )
+    const services = useServices({ serviceClass: SRV_SETTINGS })
 
     return (
         <>
             {<ConnectAlert serviceClass={SRV_SETTINGS} />}
-            {!services.length && (
-                <Alert severity="info">
-                    We could not find any device with the settings storage
-                    service on the bus!
-                </Alert>
-            )}
             <Grid container spacing={2}>
                 {services.map(service => (
                     <Grid key={service.id} item xs={12}>
