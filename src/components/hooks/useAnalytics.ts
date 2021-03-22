3cc328931d3914f15b6109530b4fb6142d613941
peli <pelikhan@users.noreply.github.com>
2021-03-22 04:47:35 -0700

    don't call analystics unless a key is present

--- src/components/hooks/useAnalytics.ts
+++ src/components/hooks/useAnalytics.ts
@@ -1,33 +1,46 @@
-import { ApplicationInsights } from '@microsoft/applicationinsights-web-basic'
+import { ApplicationInsights } from "@microsoft/applicationinsights-web-basic"
 
-const appInsights = new ApplicationInsights({
-  instrumentationKey: 'YOUR_INSTRUMENTATION_KEY_GOES_HERE',
-  isStorageUseDisabled: true,
-  isCookieUseDisabled: true,
-});
-const page = typeof window !== "undefined"
-  ? () => appInsights.track({
-    name: window.location.href,
-    time: new Date().toUTCString(),
-    baseType: "PageData"
-  })
-  : () => { };
-const track = typeof window !== "undefined"
-  ? (name: string, properties?: { [key: string]: any }) => appInsights.track({
-    name,
-    time: new Date().toUTCString(),
-    data: properties,
-    baseType: "EventData"
-  }) : (name, properties) => { };
+// YOUR_INSTRUMENTATION_KEY_GOES_HERE
+const instrumentationKey: string = undefined;
+const appInsights =
+    instrumentationKey &&
+    new ApplicationInsights({
+        instrumentationKey,
+        isStorageUseDisabled: true,
+        isCookieUseDisabled: true,
+    })
+const page =
+    typeof window !== "undefined"
+        ? () =>
+              appInsights?.track({
+                  name: window.location.href,
+                  time: new Date().toUTCString(),
+                  baseType: "PageData",
+              })
+        : () => {}
+const track =
+    typeof window !== "undefined"
+        ? // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          (name: string, properties?: { [key: string]: any }) =>
+              appInsights?.track({
+                  name,
+                  time: new Date().toUTCString(),
+                  data: properties,
+                  baseType: "EventData",
+              })
+        : // eslint-disable-next-line @typescript-eslint/no-unused-vars
+          (name, properties) => {}
 if (typeof window !== "undefined") {
-  (window as any).analytics = {
-    page, track
-  }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (window as any).analytics = {
+        page,
+        track,
+    }
 }
 
 export default function useAnalytics() {
-  return {
-    page,
-    track
-  }
+    return {
+        page,
+        track,
+    }
 }
