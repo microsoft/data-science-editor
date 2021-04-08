684d8b9a6fa26040a5bf47d2d72d60fa50574f51
pelikhan <jhalleux@microsoft.com>
2021-04-08 09:17:01 -0700

    use reports for bit:radio

--- src/components/dashboard/DashboardBitRadio.tsx
+++ src/components/dashboard/DashboardBitRadio.tsx
@@ -1,14 +1,12 @@
 import React, { useEffect, useState } from "react"
 import { DashboardServiceProps } from "./DashboardServiceWidget"
-import { BitRadioEvent } from "../../../jacdac-ts/jacdac-spec/dist/specconstants"
-import { EVENT } from "../../../jacdac-ts/src/jdom/constants"
+import { BitRadioCmd } from "../../../jacdac-ts/jacdac-spec/dist/specconstants"
+import { REPORT_RECEIVE } from "../../../jacdac-ts/src/jdom/constants"
 import { jdunpack, PackedValues } from "../../../jacdac-ts/src/jdom/pack"
+import Packet from "../../../jacdac-ts/src/jdom/packet"
 
 export default function DashboardBitRadio(props: DashboardServiceProps) {
     const { service } = props
-    const numberReceivedEvent = service.event(BitRadioEvent.NumberReceived)
-    const stringReceivedEvent = service.event(BitRadioEvent.StringReceived)
-    const bufferReceivedEvent = service.event(BitRadioEvent.BufferReceived)
     const [lastEvents, setLastEvents] = useState<
         {
             time: number
@@ -19,6 +17,8 @@ export default function DashboardBitRadio(props: DashboardServiceProps) {
     >([])
 
     const appendMessage = (data: PackedValues) => {
+        if (!data) return
+
         const [time, deviceSerial, rssi, ...payload] = data
         const evs = lastEvents.slice(0)
         const msg = { time, deviceSerial, rssi, payload }
@@ -29,30 +29,23 @@ export default function DashboardBitRadio(props: DashboardServiceProps) {
 
     useEffect(
         () =>
-            numberReceivedEvent.subscribe(EVENT, () =>
-                appendMessage(
-                    jdunpack(numberReceivedEvent.data, "u32 u32 i8 x[3] f64 s")
-                )
-            ),
-        [numberReceivedEvent, lastEvents]
-    )
-    useEffect(
-        () =>
-            stringReceivedEvent.subscribe(EVENT, () =>
-                appendMessage(
-                    jdunpack(stringReceivedEvent.data, "u32 u32 i8 x[1] s")
-                )
-            ),
-        [stringReceivedEvent, lastEvents]
-    )
-    useEffect(
-        () =>
-        bufferReceivedEvent.subscribe(EVENT, () =>
-                appendMessage(
-                    jdunpack(bufferReceivedEvent.data, "u32 u32 i8 x[1] b")
-                )
-            ),
-        [bufferReceivedEvent, lastEvents]
+            service.subscribe(REPORT_RECEIVE, (pkt: Packet) => {
+                let values: PackedValues
+                const { data, serviceCommand } = pkt
+                switch (serviceCommand) {
+                    case BitRadioCmd.NumberReceived:
+                        values = jdunpack(data, "u32 u32 i8 x[3] f64 s")
+                        break
+                    case BitRadioCmd.StringReceived:
+                        values = jdunpack(data, "u32 u32 i8 x[1] s")
+                        break
+                    case BitRadioCmd.BufferReceived:
+                        values = jdunpack(data, "u32 u32 i8 x[1] b")
+                        break
+                }
+                appendMessage(values)
+            }),
+        [service, lastEvents]
     )
 
     return (
