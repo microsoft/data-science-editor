2454641831e8406c8a2778bf25d36dfa09b98586
pelikhan <jhalleux@microsoft.com>
2021-04-22 07:31:43 -0700

    forcingly refresh page on update ready

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,23 +1,30 @@
 import Layout from "./src/components/layout"
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react"
+import ReactDOM from "react-dom"
 
 export const onRouteUpdate = ({ location }, options) => {
-  if (window.analytics)
-    window.analytics.page();
+    if (window.analytics) window.analytics.page()
+}
+
+export const onServiceWorkerUpdateReady = () => {
+    // force reload
+    console.debug(`offline: update ready, reloading...`)
+    window.location.reload()
 }
 
 export const wrapPageElement = Layout
 
-const activeEnv = process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || 'development'
-const isDev = activeEnv === 'development'
-
-/*
+// inject React Axe into DOM tree at development time
+/* blocked by crypto import issue
 export const onInitialClientRender = () => {
-  if (isDev) {
-    import("@axe-core/react").then(reactAxe => {
-      reactAxe.default(React, ReactDOM, 1000, {})
-    });
-  }
+    const activeEnv =
+        process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
+    const isDev = activeEnv === "development"
+    if (isDev) {
+        console.log(`axe: injecting into DOM`)
+        import("@axe-core/react").then(reactAxe => {
+            reactAxe.default(React, ReactDOM, 1000, {})
+        })
+    }
 }
 */
