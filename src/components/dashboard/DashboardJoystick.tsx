f0c99e6ad77984a03911ac34fdae0eac88ad3961
pelikhan <jhalleux@microsoft.com>
2021-04-29 11:27:03 -0700

    updated joystick rendering

--- src/components/dashboard/DashboardJoystick.tsx
+++ src/components/dashboard/DashboardJoystick.tsx
@@ -1,11 +1,11 @@
-import React, { useMemo } from "react"
+import React, { CSSProperties, useMemo, useRef, useState } from "react"
 import { DashboardServiceProps } from "./DashboardServiceWidget"
 import useServiceServer from "../hooks/useServiceServer"
 import { useRegisterUnpackedValue } from "../../jacdac/useRegisterValue"
 import JoystickServer, {
+    JOYSTICK_DPAD_BUTTONS,
     JOYSTICK_GAMEPAD_EXTRA_BUTTONS,
 } from "../../../jacdac-ts/src/servers/joystickserver"
-import JoystickWidget from "../widgets/JoystickWidget"
 import {
     JoystickButtons,
     JoystickReg,
@@ -15,39 +15,193 @@ import SvgWidget from "../widgets/SvgWidget"
 import useWidgetTheme from "../widgets/useWidgetTheme"
 import useSvgButtonProps from "../hooks/useSvgButtonProps"
 import LoadingProgress from "../ui/LoadingProgress"
-
-function Thumb(props: DashboardServiceProps) {
-    const { service } = props
-    const register = service.register(JoystickReg.Direction)
-    const [buttons, x, y] = useRegisterUnpackedValue<
-        [JoystickButtons, number, number]
-    >(register, props)
-    const server = useServiceServer<JoystickServer>(service)
-    const color = server ? "secondary" : "primary"
-
-    const values = () => server.reading.values()
-    const onUpdate = (buttons: JoystickButtons, newx: number, newy: number) => {
-        server.reading.setValues([buttons, newx, newy])
-        register.refresh()
-    }
-
-    return (
-        <JoystickWidget
-            x={x}
-            y={y}
-            buttons={buttons}
-            color={color}
-            onUpdate={onUpdate}
-            hostValues={server && values}
-        />
-    )
-}
+import useAnimationFrame from "../hooks/useAnimationFrame"
 
 const buttonLabels = {
     [JoystickButtons.Left]: "\u25C4",
     [JoystickButtons.Up]: "\u25B2",
     [JoystickButtons.Down]: "\u25BC",
     [JoystickButtons.Right]: "\u25BA",
+}
+
+function decay(value: number, rate: number, precision: number) {
+    let nv = value * rate
+    if (Math.abs(nv) < precision) nv = 0
+    return nv
+}
+
+function JoystickWidget(props: DashboardServiceProps) {
+    const { service } = props
+    const register = service.register(JoystickReg.Direction)
+    const [, x, y] = useRegisterUnpackedValue<
+        [JoystickButtons, number, number]
+    >(register, props)
+    const server = useServiceServer<JoystickServer>(service)
+    const color = server ? "secondary" : "primary"
+
+    const hostValues = (): [number, number] => {
+        const [, x, y] = server.reading.values()
+        return [x, y]
+    }
+    const onUpdate = (newx: number, newy: number) => {
+        const [buttons] = server.reading.values()
+        server.reading.setValues([buttons, newx, newy])
+        register.refresh()
+    }
+    const { active, background, controlBackground } = useWidgetTheme(color)
+    const dragSurfaceRef = useRef<SVGCircleElement>()
+    const [grabbing, setGrabbing] = useState(false)
+
+    const w = 40
+    const w2 = w >> 1
+    const cx = w2
+    const cy = w2
+    const rp = 2
+    const rc = 6
+    const rj = 10
+    const pw = 12
+    const ph = 8
+
+    const jx = cx + (x || 0) * rj
+    const jy = cy + (y || 0) * rj
+    const jw = 1
+
+    const updateJoystickDrag = (x: number, y: number) => {
+        const bounds = dragSurfaceRef.current.getBoundingClientRect()
+
+        const dx = (x - bounds.left) * (w / bounds.width) - w2
+        const dy = (y - bounds.top) * (w / bounds.height) - w2
+
+        const angle = Math.atan2(dy, dx)
+        const distance =
+            Math.min(Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2)), rj) / rj
+
+        const newx = distance * Math.cos(angle)
+        const newy = distance * Math.sin(angle)
+
+        onUpdate(newx, newy)
+    }
+
+    const handlePointerDown = async (
+        ev: React.PointerEvent<SVGCircleElement>
+    ) => {
+        ev.preventDefault()
+        setGrabbing(true)
+        updateJoystickDrag(ev.clientX, ev.clientY)
+    }
+    const handlePointerUp = (ev: React.PointerEvent<SVGCircleElement>) => {
+        ev.preventDefault()
+        setGrabbing(false)
+    }
+    const handlePointerMove = async (
+        ev: React.PointerEvent<SVGCircleElement>
+    ) => {
+        ev.preventDefault()
+        if (grabbing) updateJoystickDrag(ev.clientX, ev.clientY)
+    }
+
+    useAnimationFrame(() => {
+        if (!server || grabbing) return false // let use do its thing
+        const [ax, ay] = hostValues()
+        const nx = decay(ax, 0.9, 16)
+        const ny = decay(ay, 0.9, 16)
+
+        // async
+        onUpdate(nx, ny)
+
+        return nx !== 0 || ny !== 0
+    }, [server, grabbing])
+
+    const handleStyle: CSSProperties = {
+        cursor: grabbing ? "grabbing" : "grab",
+    }
+
+    return (
+        <>
+            <circle
+                ref={dragSurfaceRef}
+                className="joystick-background"
+                cx={cx}
+                cy={cy}
+                r="16"
+                fill={background}
+            ></circle>
+            <rect
+                className="dpad-up"
+                x={"16"}
+                y="6"
+                width={ph}
+                height={pw}
+                rx={rp}
+                fill={controlBackground}
+            ></rect>
+            <rect
+                className="dpad-down"
+                x="16"
+                y="22"
+                width={ph}
+                height={pw}
+                rx={rp}
+                fill={controlBackground}
+            ></rect>
+            <rect
+                className="dpad-right"
+                x="22"
+                y="16"
+                width={pw}
+                height={ph}
+                ry={rp}
+                fill={controlBackground}
+            ></rect>
+            <rect
+                className="dpad-left"
+                x="6"
+                y="16"
+                width={pw}
+                height={ph}
+                ry={rp}
+                fill={controlBackground}
+            ></rect>
+            <circle
+                className="dpad-center"
+                cx={cx}
+                cy={cy}
+                r={rc}
+                fill={controlBackground}
+            ></circle>
+            {server ? (
+                <circle
+                    className="joystick-handle"
+                    cx={jx}
+                    cy={jy}
+                    r={rc}
+                    fill={background}
+                    stroke={active}
+                    strokeWidth={jw}
+                    tabIndex={0}
+                    role="button"
+                    aria-label="joystick handle"
+                    arial-live="polite"
+                    onPointerMove={handlePointerMove}
+                    onPointerDown={handlePointerDown}
+                    onPointerUp={handlePointerUp}
+                    onPointerLeave={handlePointerUp}
+                    style={handleStyle}
+                />
+            ) : (
+                <circle
+                    className="joystick-handle"
+                    cx={jx}
+                    cy={jy}
+                    r={rc}
+                    fill={background}
+                    stroke={active}
+                    strokeWidth={jw}
+                    aria-label="joystick handle"
+                />
+            )}
+        </>
+    )
 }
 
 function ArcadeButton(props: {
@@ -105,14 +259,26 @@ function ArcadeButton(props: {
     )
 }
 
-function Gamepad(props: DashboardServiceProps) {
+export default function DashboardJoystick(props: DashboardServiceProps) {
     const { service } = props
+    let [variant] = useRegisterUnpackedValue<[JoystickVariant]>(
+        service.register(JoystickReg.Variant),
+        props
+    )
     const [buttonsAvailable] = useRegisterUnpackedValue<[JoystickButtons]>(
         service.register(JoystickReg.ButtonsAvailable),
         props
     )
+
+    // if variant is not specific, infer from buttons
+    if (variant === undefined) {
+        if (buttonsAvailable & JOYSTICK_GAMEPAD_EXTRA_BUTTONS)
+            variant = JoystickVariant.Gamepad
+        else if (!buttonsAvailable || buttonsAvailable === JoystickButtons.A)
+            variant = JoystickVariant.Thumb
+    }
     const directionRegister = service.register(JoystickReg.Direction)
-    const [buttons, x, y] = useRegisterUnpackedValue<
+    const [buttons] = useRegisterUnpackedValue<
         [JoystickButtons, number, number]
     >(directionRegister, props)
     const server = useServiceServer<JoystickServer>(service)
@@ -120,6 +286,8 @@ function Gamepad(props: DashboardServiceProps) {
     const { background } = useWidgetTheme(color)
 
     // buttonsAvailable should be defined by now
+    const analog = !(buttonsAvailable & JOYSTICK_DPAD_BUTTONS)
+    const hasButtons = !!(buttonsAvailable & ~JOYSTICK_DPAD_BUTTONS)
 
     const w = 256
     const h = 128
@@ -188,28 +356,44 @@ function Gamepad(props: DashboardServiceProps) {
     const aby = ch * 3 + 4
     const abr = cw / 2
     const abw = cw * 5 - 6
+    const padcx = 3 * cw
+    const padcy = 2 * ch
+    const padr = 2.6 * cw
+
+    // need button info
+    if (buttonsAvailable === undefined) return <LoadingProgress />
+
     return (
         <SvgWidget width={w} height={h}>
-            <circle
-                cx={cw * 3}
-                cy={2 * ch}
-                r={2.6 * cw}
-                fill="none"
-                stroke={background}
-                strokeWidth={4}
-            />
-            <rect
-                transform={`rotate(-66, ${abx}, ${aby})`}
-                x={abx}
-                y={aby}
-                rx={abr}
-                ry={abr}
-                width={abw}
-                height={cw * 2.2}
-                fill="none"
-                stroke={background}
-                strokeWidth={4}
-            />
+            {!analog && (
+                <circle
+                    cx={padcx}
+                    cy={padcy}
+                    r={padr}
+                    fill="none"
+                    stroke={background}
+                    strokeWidth={4}
+                />
+            )}
+            {analog && (
+                <g transform={`scale(${padr / 16}) translate(${-1.8},${-1.8})`}>
+                    <JoystickWidget {...props} />
+                </g>
+            )}
+            {hasButtons && (
+                <rect
+                    transform={`rotate(-66, ${abx}, ${aby})`}
+                    x={abx}
+                    y={aby}
+                    rx={abr}
+                    ry={abr}
+                    width={abw}
+                    height={cw * 2.2}
+                    fill="none"
+                    stroke={background}
+                    strokeWidth={4}
+                />
+            )}
             {pos.map(({ id, cx, cy, small }) => (
                 <ArcadeButton
                     key={id}
@@ -225,35 +409,5 @@ function Gamepad(props: DashboardServiceProps) {
                 />
             ))}
         </SvgWidget>
-    )
-}
-
-export default function DashboardJoystick(props: DashboardServiceProps) {
-    const { service } = props
-    let [variant] = useRegisterUnpackedValue<[JoystickVariant]>(
-        service.register(JoystickReg.Variant),
-        props
-    )
-    const [buttonsAvailable] = useRegisterUnpackedValue<[JoystickButtons]>(
-        service.register(JoystickReg.ButtonsAvailable),
-        props
-    )
-
-    // need button info
-    if (buttonsAvailable === undefined) return <LoadingProgress />
-
-    // if variant is not specific, infer from buttons
-    if (variant === undefined) {
-        if (buttonsAvailable & JOYSTICK_GAMEPAD_EXTRA_BUTTONS)
-            variant = JoystickVariant.Gamepad
-        else if (!buttonsAvailable || buttonsAvailable === JoystickButtons.A)
-            variant = JoystickVariant.Thumb
-    }
-
-    // render in 2 modes
-    return variant == JoystickVariant.Thumb ? (
-        <Thumb {...props} />
-    ) : (
-        <Gamepad {...props} />
     )
 }
