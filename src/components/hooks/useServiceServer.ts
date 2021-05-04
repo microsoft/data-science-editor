5d104f612acd9e4f4c9bcad9282a828a5417e847
pelikhan <jhalleux@microsoft.com>
2021-05-04 15:11:36 -0700

    use single twin server for lifetime of service

--- src/components/hooks/useServiceServer.ts
+++ src/components/hooks/useServiceServer.ts
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react"
+import { useMemo } from "react"
 import { JDService } from "../../../jacdac-ts/src/jdom/service"
 import JDServiceServer from "../../../jacdac-ts/src/jdom/serviceserver"
 import useServiceProvider from "./useServiceProvider"
@@ -8,18 +8,15 @@ export default function useServiceServer<T extends JDServiceServer>(
     createTwin?: () => T
 ) {
     const provider = useServiceProvider(service.device)
-    const twin = useMemo<T>(() => createTwin?.(), [])
-    useEffect(() => {
-        if (!provider && twin) {
-            //console.log(`set twin`, { twin, service })
-            twin.twin = service
+    const twin = useMemo<T>(() => {
+        if (provider) return undefined
+        let twin = service.twin as T
+        if (!twin && createTwin) {
+            twin = createTwin()
+            console.debug(`create twin`, { twin })
+            if (twin) service.twin = twin
         }
-        return () => {
-            if (twin) {
-                //console.log(`clean twin`, twin)
-                twin.twin = undefined
-            }
-        }
-    }, [service, provider])
+        return twin
+    }, [service, provider, service?.changeId])
     return (provider?.service(service?.serviceIndex) as T) || twin
 }
