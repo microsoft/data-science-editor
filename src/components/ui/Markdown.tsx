ba64e04ad83fa68f57332cf8d3ced7a4b18ad991
pelikhan <jhalleux@microsoft.com>
2021-05-06 11:20:44 -0700

    fix react-markdown

--- src/components/ui/Markdown.tsx
+++ src/components/ui/Markdown.tsx
@@ -1,6 +1,10 @@
 import React from "react"
-import ReactMarkdown from 'react-markdown'
+import ReactMarkdown from "react-markdown"
 
-export default function Markdown(props: { source: string, className?: string }) {
-    return <ReactMarkdown {...props} />
+export default function Markdown(props: {
+    source: string
+    className?: string
+}) {
+    const { source, ...others } = props
+    return <ReactMarkdown {...others}>{source}</ReactMarkdown>
 }
