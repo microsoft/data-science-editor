6d1a1ed993abe78c12d666f4516f0d951756545c
pelikhan <jhalleux@microsoft.com>
2021-05-30 21:11:28 -0700

    refactor twin field

---
+++ src/components/vm/fields/ReactInlineField.tsx
@@ -0,0 +1,88 @@
+import React, { ReactNode } from "react"
+import ReactDOM from "react-dom"
+import ReactField from "./ReactField"
+import { child } from "../../widgets/svg"
+import DarkModeProvider from "../../ui/DarkModeProvider"
+import { IdProvider } from "react-use-id-hook"
+import JacdacProvider from "../../../jacdac/Provider"
+import AppTheme from "../../ui/AppTheme"
+import Blockly from "blockly"
+import { WorkspaceProvider } from "../WorkspaceContext"
+
+export default class ReactInlineField<T> extends ReactField<T> {
+    protected container: HTMLDivElement
+    protected resizeObserver: ResizeObserver
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    constructor(options?: any) {
+        super(options?.value, undefined, options, { width: 240, height: 160 })
+    }
+
+    protected initCustomView() {
+        const { width, height } = this.size_
+        const fo = child(this.fieldGroup_, "foreignObject", {
+            x: 0,
+            y: 0,
+            width,
+            height,
+        }) as SVGForeignObjectElement
+
+        this.container = document.createElement("div")
+        fo.appendChild(this.container)
+
+        this.resizeObserver = new ResizeObserver(
+            (entries: ResizeObserverEntry[]) => {
+                const entry = entries[0]
+                const { contentRect } = entry
+                this.size_.width = contentRect.width
+                this.size_.height = contentRect.height
+                fo.setAttribute("width", this.size_.width + "")
+                fo.setAttribute("height", this.size_.height + "")
+                this.forceRerender()
+            }
+        )
+        this.resizeObserver.observe(this.container)
+
+        ReactDOM.render(this.renderBlock(), this.container)
+        return fo
+    }
+
+    dispose() {
+        if (this.container) {
+            ReactDOM.unmountComponentAtNode(this.container)
+            this.container = undefined
+        }
+        if (this.resizeObserver) {
+            this.resizeObserver.disconnect()
+            this.resizeObserver = undefined
+        }
+        super.dispose()
+    }
+
+    renderField(): ReactNode {
+        return <div>not used</div>
+    }
+
+    renderInlineField(): ReactNode {
+        return null
+    }
+
+    renderBlock(): ReactNode {
+        return (
+            <WorkspaceProvider field={this}>
+                <DarkModeProvider fixedDarkMode="dark">
+                    <IdProvider>
+                        <JacdacProvider>
+                            <AppTheme>{this.renderInlineField()}</AppTheme>
+                        </JacdacProvider>
+                    </IdProvider>
+                </DarkModeProvider>
+            </WorkspaceProvider>
+        )
+    }
+
+    // don't bind any mouse event
+    bindEvents_() {
+        Blockly.Tooltip.bindMouseEvents(this.getClickTarget_())
+    }
+}
--- src/components/vm/fields/TwinField.tsx
+++ src/components/vm/fields/TwinField.tsx
@@ -1,20 +1,13 @@
-import React, { PointerEvent, ReactNode, useContext } from "react"
-import ReactDOM from "react-dom"
-import ReactField, { ReactFieldJSON } from "./ReactField"
-import { child } from "../../widgets/svg"
-import DarkModeProvider from "../../ui/DarkModeProvider"
-import { IdProvider } from "react-use-id-hook"
-import JacdacProvider from "../../../jacdac/Provider"
-import AppTheme from "../../ui/AppTheme"
+import React, { PointerEvent, useContext } from "react"
+import { ReactFieldJSON } from "./ReactField"
 import { Button, Grid } from "@material-ui/core"
 import DashboardServiceWidget from "../../dashboard/DashboardServiceWidget"
 import AddIcon from "@material-ui/icons/Add"
 import { startServiceProviderFromServiceClass } from "../../../../jacdac-ts/src/servers/servers"
 import JacdacContext, { JacdacContextProps } from "../../../jacdac/Context"
 import Alert from "../../ui/Alert"
-import Blockly from "blockly"
 import { serviceSpecificationFromClassIdentifier } from "../../../../jacdac-ts/src/jdom/spec"
-import WorkspaceContext, { WorkspaceProvider } from "../WorkspaceContext"
+import WorkspaceContext from "../WorkspaceContext"
 
 function TwinWidget(props: { serviceClass: number }) {
     const { serviceClass } = props
@@ -74,12 +67,10 @@ function TwinWidget(props: { serviceClass: number }) {
     )
 }
 
-export default class TwinField extends ReactField<number> {
+export default class TwinField extends ReactInlineField<number> {
     static KEY = "jacdac_field_twin"
     static EDITABLE = false
     protected serviceClass: number
-    protected container: HTMLDivElement
-    protected resizeObserver: ResizeObserver
 
     static fromJson(options: ReactFieldJSON) {
         return new TwinField(options)
@@ -91,69 +82,7 @@ export default class TwinField extends ReactField<number> {
         this.serviceClass = options.serviceClass
     }
 
-    protected initCustomView() {
-        const { width, height } = this.size_
-        const fo = child(this.fieldGroup_, "foreignObject", {
-            x: 0,
-            y: 0,
-            width,
-            height,
-        }) as SVGForeignObjectElement
-
-        this.container = document.createElement("div")
-        fo.appendChild(this.container)
-
-        this.resizeObserver = new ResizeObserver(
-            (entries: ResizeObserverEntry[]) => {
-                const entry = entries[0]
-                const { contentRect } = entry
-                this.size_.width = contentRect.width
-                this.size_.height = contentRect.height
-                fo.setAttribute("width", this.size_.width + "")
-                fo.setAttribute("height", this.size_.height + "")
-                this.forceRerender()
-            }
-        )
-        this.resizeObserver.observe(this.container)
-
-        ReactDOM.render(this.renderBlock(), this.container)
-        return fo
-    }
-
-    dispose() {
-        if (this.container) {
-            ReactDOM.unmountComponentAtNode(this.container)
-            this.container = undefined
-        }
-        if (this.resizeObserver) {
-            this.resizeObserver.disconnect()
-            this.resizeObserver = undefined
-        }
-        super.dispose()
-    }
-
-    renderField(): ReactNode {
-        return <div>field</div>
-    }
-
-    renderBlock(): ReactNode {
-        return (
-            <WorkspaceProvider field={this}>
-                <DarkModeProvider fixedDarkMode="dark">
-                    <IdProvider>
-                        <JacdacProvider>
-                            <AppTheme>
-                                <TwinWidget serviceClass={this.serviceClass} />
-                            </AppTheme>
-                        </JacdacProvider>
-                    </IdProvider>
-                </DarkModeProvider>
-            </WorkspaceProvider>
-        )
-    }
-
-    // don't bind any mouse event
-    bindEvents_() {
-        Blockly.Tooltip.bindMouseEvents(this.getClickTarget_())
+    renderInlineField() {
+        return <TwinWidget serviceClass={this.serviceClass} />
     }
 }
