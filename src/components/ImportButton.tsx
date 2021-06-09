c83fe2f244e0d12e2d48b65630fa2f74a85b7bb3
pelikhan <jhalleux@microsoft.com>
2021-06-08 20:42:12 -0700

    lazy dropzone dialog

--- src/components/ImportButton.tsx
+++ src/components/ImportButton.tsx
@@ -1,48 +1,71 @@
-import React, { useState } from 'react';
-import { DropzoneDialog } from 'material-ui-dropzone'
-import { Box, Button } from '@material-ui/core';
+import React, { lazy, useState } from "react"
+import { Box, Button } from "@material-ui/core"
 // tslint:disable-next-line: no-submodule-imports match-default-export-name
-import OpenInBrowserIcon from '@material-ui/icons/OpenInBrowser';
-import IconButtonWithTooltip from './ui/IconButtonWithTooltip';
+import OpenInBrowserIcon from "@material-ui/icons/OpenInBrowser"
+import IconButtonWithTooltip from "./ui/IconButtonWithTooltip"
+import Suspense from "./ui/Suspense"
+const DropzoneDialog = lazy(() => import("./ui/DropzoneDialog"))
 
 export default function ImportButton(props: {
-    icon?: boolean,
-    text: string,
-    onFilesUploaded: (files: File[]) => void,
-    disabled?: boolean,
-    acceptedFiles?: string[],
+    icon?: boolean
+    text: string
+    onFilesUploaded: (files: File[]) => void
+    disabled?: boolean
+    acceptedFiles?: string[]
     filesLimit?: number
     className?: string
 }) {
-    const { text, onFilesUploaded, disabled, acceptedFiles, icon, filesLimit, className } = props;
+    const {
+        text,
+        onFilesUploaded,
+        disabled,
+        acceptedFiles,
+        icon,
+        filesLimit,
+        className,
+    } = props
     const [open, setOpen] = useState(false)
 
     const handleOpen = () => {
         setOpen(true)
     }
     const handleSave = (files: File[]) => {
-        if (files?.length)
-            onFilesUploaded(files)
+        if (files?.length) onFilesUploaded(files)
         setOpen(false)
     }
     const handleClose = () => setOpen(false)
 
-    return <Box className={className}>
-        {icon && <IconButtonWithTooltip title={text} onClick={handleOpen}>
-            <OpenInBrowserIcon />
-        </IconButtonWithTooltip>}
-        {!icon && <Button disabled={disabled} variant="outlined" onClick={handleOpen} startIcon={<OpenInBrowserIcon />}>
-            {text}
-        </Button>}
-        <DropzoneDialog
-            open={open}
-            onSave={handleSave}
-            maxFileSize={5000000}
-            onClose={handleClose}
-            acceptedFiles={acceptedFiles}
-            clearOnUnmount={true}
-            filesLimit={filesLimit || 1}
-            submitButtonText={"import"}
-        />
-    </Box>
+    return (
+        <Box className={className}>
+            {icon && (
+                <IconButtonWithTooltip title={text} onClick={handleOpen}>
+                    <OpenInBrowserIcon />
+                </IconButtonWithTooltip>
+            )}
+            {!icon && (
+                <Button
+                    disabled={disabled}
+                    variant="outlined"
+                    onClick={handleOpen}
+                    startIcon={<OpenInBrowserIcon />}
+                >
+                    {text}
+                </Button>
+            )}
+            {open && (
+                <Suspense>
+                    <DropzoneDialog
+                        open={open}
+                        onSave={handleSave}
+                        maxFileSize={5000000}
+                        onClose={handleClose}
+                        acceptedFiles={acceptedFiles}
+                        clearOnUnmount={true}
+                        filesLimit={filesLimit || 1}
+                        submitButtonText={"import"}
+                    />
+                </Suspense>
+            )}
+        </Box>
+    )
 }
---
+++ src/components/ui/DropzoneDialog.ts
@@ -0,0 +1,3 @@
+import { DropzoneDialog } from "material-ui-dropzone"
+
+export default DropzoneDialog
