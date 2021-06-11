69703b747fc09dba6a58de81b6d5e5473c69f799
pelikhan <jhalleux@microsoft.com>
2021-06-11 09:50:12 -0700

    updated variable tables

--- src/components/blockly/fields/VariablesFields.tsx
+++ src/components/blockly/fields/VariablesFields.tsx
@@ -1,34 +1,56 @@
-import { useTheme } from "@material-ui/core"
+import { createStyles, makeStyles, Theme, useTheme } from "@material-ui/core"
 import React, { useContext, useEffect, useState } from "react"
-import { GLOBAL_CHANGE } from "../../../../jacdac-ts/src/vm/environment"
+import { VM_GLOBAL_CHANGE } from "../../../../jacdac-ts/src/vm/events"
 import { atomic } from "../../../../jacdac-ts/src/vm/runner"
 import WorkspaceContext from "../WorkspaceContext"
 import { ReactFieldJSON } from "./ReactField"
 import ReactInlineField from "./ReactInlineField"
 
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        table: {
+            padding: 0,
+            margin: 0,
+            fontSize: "0.9rem",
+            lineHeight: "1rem",
+            color: theme.palette.text.primary,
+
+            "& td": {
+                borderColor: "#ccc",
+            },
+        },
+    })
+)
+
 function VariablesWidget() {
     const { runner } = useContext(WorkspaceContext)
-    const theme = useTheme()
+    const classes = useStyles()
     const [variables, setVariables] = useState<
         { name: string; value: atomic }[]
     >(runner?.globals())
     useEffect(
         () =>
-            runner?.subscribe(GLOBAL_CHANGE, () =>
+            runner?.subscribe(VM_GLOBAL_CHANGE, () =>
                 setVariables(runner.globals())
             ),
         [runner]
     )
 
     return (
-        <table style={{ color: theme.palette.text.primary }}>
-            {variables?.map(({ name, value }) => (
-                <tr key={name}>
-                    <td>{name}</td>
-                    <td>{value}</td>
-                </tr>
-            ))}
-        </table>
+        <>
+            {variables && (
+                <table className={classes.table}>
+                    <tbody>
+                        {variables?.map(({ name, value }) => (
+                            <tr key={name}>
+                                <td>{name}</td>
+                                <td>{value}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
+        </>
     )
 }
 
