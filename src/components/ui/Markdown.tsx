f32695795fa063f69cbf3447e234b9d023d5a6b6
pelikhan <jhalleux@microsoft.com>
2021-06-11 13:42:08 -0700

    adding component info

--- src/components/ui/Markdown.tsx
+++ src/components/ui/Markdown.tsx
@@ -1,9 +1,14 @@
 import React from "react"
 import ReactMarkdown from "react-markdown"
+import {
+    NormalComponents,
+    SpecialComponents,
+} from "react-markdown/src/ast-to-react"
 
 export default function Markdown(props: {
     source: string
     className?: string
+    components?: Partial<NormalComponents | SpecialComponents>
 }) {
     const { source, ...others } = props
     return <ReactMarkdown {...others}>{source}</ReactMarkdown>
