83cf79733ec56acb4dde1490c352edd6c4cf99c3
pelikhan <jhalleux@microsoft.com>
2021-06-28 17:28:25 +0200

    removed logging

--- src/components/blockly/dsl/workers/csv.proxy.ts
+++ src/components/blockly/dsl/workers/csv.proxy.ts
@@ -6,12 +6,10 @@ import {
 } from "../../../../workers/csv/dist/node_modules/csv.worker"
 
 export default async function downloadCSV(url: string): Promise<CsvFile> {
-    console.log(`download csv`, { url })
     const worker = workerProxy("csv")
     const res = await worker.postMessage<CsvRequest, CsvResponse>({
         worker: "csv",
         url,
     })
-    console.log(`downloaded csv`, res)
     return res?.file
 }
