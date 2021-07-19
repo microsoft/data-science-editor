6b02b5077bbacab5eaf0ccf0f98bcd5169f1dc24
pelikhan <jhalleux@microsoft.com>
2021-07-19 09:29:13 -0700

    more a11y fixes

--- src/components/ui/TabPanel.tsx
+++ src/components/ui/TabPanel.tsx
@@ -1,14 +1,14 @@
 import React from "react"
-import { Box, Typography } from "@material-ui/core";
+import { Box, Typography } from "@material-ui/core"
 import { useId } from "react-use-id-hook"
 
 export default function TabPanel(props: {
-    children?: React.ReactNode;
-    index: any;
-    value: any;
+    children?: React.ReactNode
+    index: any
+    value: any
 }) {
-    const { children, value, index, ...other } = props;
-    const did = useId();
+    const { children, value, index, ...other } = props
+    const did = useId()
 
     return (
         <div
@@ -24,12 +24,5 @@ export default function TabPanel(props: {
                 </Box>
             )}
         </div>
-    );
-}
-
-export function a11yProps(index: any) {
-    return {
-        id: `simple-tab-${index}`,
-        'aria-controls': `simple-tabpanel-${index}`,
-    };
+    )
 }
