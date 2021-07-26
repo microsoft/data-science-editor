89cc990aa4ec6b3b2d2bd065143f1e993527868a
pelikhan <jhalleux@microsoft.com>
2021-07-26 13:13:42 -0700

    use firmware identifier value

--- src/jacdac/useDeviceSpecification.ts
+++ src/jacdac/useDeviceSpecification.ts
@@ -1,16 +1,9 @@
-import { ControlReg } from "../../jacdac-ts/src/jdom/constants"
 import { JDDevice } from "../../jacdac-ts/src/jdom/device"
 import { deviceSpecificationFromFirmwareIdentifier } from "../../jacdac-ts/src/jdom/spec"
 import useChange from "./useChange"
-import { useRegisterUnpackedValue } from "./useRegisterValue"
 
 export default function useDeviceSpecification(device: JDDevice) {
-    const firmwareIdentifierRegister = useChange(device, _ =>
-        device?.service(0)?.register(ControlReg.FirmwareIdentifier)
-    )
-    const [firmwareIdentifier] = useRegisterUnpackedValue<[number]>(
-        firmwareIdentifierRegister
-    )
+    const firmwareIdentifier = useChange(device, _ => _?.firmwareIdentifier)
     const specification =
         deviceSpecificationFromFirmwareIdentifier(firmwareIdentifier)
     return specification
