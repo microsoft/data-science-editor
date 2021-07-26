1a0f007236f8bb6b5d2dd09698b6838bc9375e27
pelikhan <jhalleux@microsoft.com>
2021-07-26 07:15:10 -0700

    add empty option to unset selected column

--- src/components/blockly/fields/DataColumnChooserField.ts
+++ src/components/blockly/fields/DataColumnChooserField.ts
@@ -36,7 +36,9 @@ export default class DataColumnChooserField extends FieldDropdown {
                 .filter((h, i) => !this.dataType || types[i] === this.dataType)
                 .map(h => [h, h]) || []
         const value = this.getValue()
-        return options.length < 1 ? [[value || "", value || ""]] : options
+        return options.length < 1
+            ? [[value || "", value || ""]]
+            : [...options, ["", ""]]
     }
 
     doClassValidation_(newValue?: string) {
