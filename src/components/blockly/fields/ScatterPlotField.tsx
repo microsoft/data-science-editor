2e11795e1bb52181d77c2578ee8dbdc521b6d6e7
pelikhan <jhalleux@microsoft.com>
2021-07-27 13:52:45 -0700

    moving to vega-lite scatter plot

--- src/components/blockly/fields/ScatterPlotField.tsx
+++ src/components/blockly/fields/ScatterPlotField.tsx
@@ -1,89 +1,28 @@
-import React, { lazy, useContext } from "react"
+import React, { useContext } from "react"
 import WorkspaceContext from "../WorkspaceContext"
 import { ReactFieldJSON } from "./ReactField"
 import ReactInlineField from "./ReactInlineField"
-import useBlockChartProps from "../useBlockChartProps"
 import useBlockData from "../useBlockData"
-import { PointerBoundary } from "./PointerBoundary"
-import Suspense from "../../ui/Suspense"
-import { NoSsr } from "@material-ui/core"
-import { tidyToNivo } from "./nivo"
-import {
-    ANIMATE_MAX_ITEMS,
-    CHART_HEIGHT,
-    CHART_WIDTH,
-    SCATTER_MAX_ITEMS,
-} from "../toolbox"
-const ScatterPlot = lazy(() => import("./ScatterPlot"))
+import type { VisualizationSpec } from "react-vega"
+import VegaLiteWidget from "./VegaLiteWidget"
+import { tidyResolveHeader } from "./tidy"
 
-function ScatterChartWidget() {
-    const { sourceBlock, dragging } = useContext(WorkspaceContext)
+function ScatterPlotWidget() {
+    const { sourceBlock } = useContext(WorkspaceContext)
     const { data } = useBlockData(sourceBlock)
-
-    // need to map data to nivo
-    const x = sourceBlock?.getFieldValue("x")
-    const y = sourceBlock?.getFieldValue("y")
-    const { series, labels } = tidyToNivo(data, [x, y], ["x", "y"], {
-        sliceSample: SCATTER_MAX_ITEMS,
-    })
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const { chartProps } = useBlockChartProps<any>(sourceBlock, {
-        colors: { scheme: "category10" },
-        data: series,
-        margin: { top: 8, right: 8, bottom: 38, left: 64 },
-        xScale: { type: "linear", min: "auto", max: "auto" },
-        yScale: { type: "linear", min: "auto", max: "auto" },
-        axisTop: null,
-        axisRight: null,
-        isInteractive: false,
-        axisBottom: {
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: x,
-            legendPosition: "middle",
-            legendOffset: 34,
+    const x = tidyResolveHeader(data, sourceBlock?.getFieldValue("x"), "number")
+    const y = tidyResolveHeader(data, sourceBlock?.getFieldValue("y"), "number")
+    if (!x || !y) return null
+    const spec: VisualizationSpec = {
+        description: `Scatter plot of ${x}x${y}`,
+        mark: "point",
+        encoding: {
+            x: { field: x, type: "quantitative" },
+            y: { field: y, type: "quantitative" },
         },
-        axisLeft: {
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: y,
-            legendPosition: "middle",
-            legendOffset: -32,
-        },
-    })
-    if (chartProps) {
-        chartProps.animate =
-            !dragging && series?.[0]?.data?.length < ANIMATE_MAX_ITEMS
-        chartProps.data = series
+        data: { name: "values" },
     }
-
-    const hasData =
-        labels?.length === 2 &&
-        labels[0] !== labels[1] &&
-        !!chartProps?.data?.[0].data?.length
-    if (!hasData) return null
-
-    chartProps.axisBottom.legend = labels[0]
-    chartProps.axisLeft.legend = labels[1]
-
-    console.log("scatter", { x, y, series, chartProps })
-    return (
-        <NoSsr>
-            <div style={{ background: "#fff", borderRadius: "0.25rem" }}>
-                <PointerBoundary>
-                    <Suspense>
-                        <ScatterPlot
-                            width={CHART_WIDTH}
-                            height={CHART_HEIGHT}
-                            {...chartProps}
-                        />
-                    </Suspense>
-                </PointerBoundary>
-            </div>
-        </NoSsr>
-    )
+    return <VegaLiteWidget spec={spec} />
 }
 
 export default class ScatterPlotField extends ReactInlineField {
@@ -100,6 +39,6 @@ export default class ScatterPlotField extends ReactInlineField {
     }
 
     renderInlineField() {
-        return <ScatterChartWidget />
+        return <ScatterPlotWidget />
     }
 }
