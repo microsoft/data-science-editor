e6f49e027b669902b2e0903ef1f18dabda35c166
Peli de Halleux <pelikhan@users.noreply.github.com>
2021-07-30 13:15:48 -0700

    Gatsby3.10 (#85)
    * update dependencies
    
    * updated ts
    
    * enable fast dev in gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,8 @@ module.exports = {
     flags: {
         PRESERVE_FILE_DOWNLOAD_CACHE: true,
         PRESERVE_WEBPACK_CACHE: true,
+        DEV_WEBPACK_CACHE: true,
+        FAST_DEV: true,
     },
     plugins: [
         `gatsby-plugin-no-sourcemaps`,
