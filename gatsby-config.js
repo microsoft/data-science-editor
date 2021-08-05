bc1bd607720f606021074e6c9c961fe15003c397
pelikhan <jhalleux@microsoft.com>
2021-08-05 06:25:36 -0700

    add description in sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -240,6 +240,7 @@ module.exports = {
             options: {
                 name: `Jacdac - plug-n-play for microcontrollers`,
                 short_name: `Jacdac`,
+                description: `Jacdac is a plug-and-play hardware and software stack for microcontrollers and their peripherals such as sensors and actuators. Jacdac is primarily designed for “modular electronics” scenarios that support rapid prototyping, creative exploration, making and learning through physical computing. Jacdac is designed to be cheap, flexible and extensible.`,
                 start_url: `/`,
                 background_color: `#ffc400`,
                 theme_color: `#ffc400`,
