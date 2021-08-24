b9c83ca0f676e5fbf47b4ef27c5c7cc89de300bf
pelikhan <jhalleux@microsoft.com>
2021-08-24 06:21:21 -0700

    add clear button in search box

--- src/components/DrawerSearchInput.tsx
+++ src/components/DrawerSearchInput.tsx
@@ -1,17 +1,29 @@
-import React, { useContext } from "react"
+import React, { useContext, useRef, useState } from "react"
 // tslint:disable-next-line: no-submodule-imports
 import TextField from "@material-ui/core/TextField"
 // tslint:disable-next-line: no-submodule-imports
 import AppContext from "./AppContext"
 import { useId } from "react-use-id-hook"
+import { InputAdornment } from "@material-ui/core"
+import ClearIcon from "@material-ui/icons/Clear"
+import IconButtonWithTooltip from "./ui/IconButtonWithTooltip"
+import { delay } from "../../jacdac-ts/src/jdom/utils"
 
 export default function DrawerSearchInput() {
     const { searchQuery, setSearchQuery } = useContext(AppContext)
+    const [focused, setFocused] = useState(false)
     const textId = useId()
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setSearchQuery(event.target.value)
     }
+    const handleClear = () => setSearchQuery("")
+    const handleFocus = () => setFocused(true)
+    const handleBlur = async () => {
+        await delay(200)
+        setFocused(false)
+    }
+
     return (
         <TextField
             id={textId}
@@ -23,6 +35,23 @@ export default function DrawerSearchInput() {
             aria-label="Search documentation"
             value={searchQuery}
             onChange={handleChange}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            InputProps={
+                focused && {
+                    endAdornment: (
+                        <InputAdornment position="end">
+                            <IconButtonWithTooltip
+                                title="clear"
+                                size="small"
+                                onClick={handleClear}
+                            >
+                                <ClearIcon />
+                            </IconButtonWithTooltip>
+                        </InputAdornment>
+                    ),
+                }
+            }
         />
     )
 }
