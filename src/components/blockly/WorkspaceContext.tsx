31eda8d760280fbf799677311dba1ac6fb2cdaee
pelikhan <jhalleux@microsoft.com>
2021-08-27 08:57:17 -0700

    fix roles

--- src/components/blockly/WorkspaceContext.tsx
+++ src/components/blockly/WorkspaceContext.tsx
@@ -246,7 +246,7 @@ export function WorkspaceProvider(props: {
     )
     const roleServiceClass = useChange(
         roleManager,
-        _ => _?.roles.find(r => r.role === role)?.serviceClass
+        _ => _?.roles().find(r => r.role === role)?.serviceClass
     )
     const [flyout, setFlyout] = useState(!!sourceBlock?.isInFlyout)
 
