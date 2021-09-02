75f46a9950e1d302ecaaa4210721f0614c0c9d2c
pelikhan <jhalleux@microsoft.com>
2021-09-01 20:28:04 -0700

    code formatting

--- src/components/ConsoleListItem.tsx
+++ src/components/ConsoleListItem.tsx
@@ -1,19 +1,26 @@
-
-import React from 'react';
-import { ListItem, Typography, ListItemIcon, Theme, makeStyles, createStyles, Chip } from '@material-ui/core';
-import Packet from '../../jacdac-ts/src/jdom/packet';
-import { decodePacketData } from '../../jacdac-ts/src/jdom/pretty'
-import clsx from 'clsx';
+import React from "react"
+import {
+    ListItem,
+    Typography,
+    ListItemIcon,
+    Theme,
+    makeStyles,
+    createStyles,
+    Chip,
+} from "@material-ui/core"
+import Packet from "../../jacdac-ts/src/jdom/packet"
+import { decodePacketData } from "../../jacdac-ts/src/jdom/pretty"
+import clsx from "clsx"
 // tslint:disable-next-line: match-default-export-name no-submodule-imports
-import WarningIcon from '@material-ui/icons/Warning';
+import WarningIcon from "@material-ui/icons/Warning"
 // tslint:disable-next-line: match-default-export-name no-submodule-imports
-import ErrorIcon from '@material-ui/icons/Error';
-import { JSONTryParse } from '../../jacdac-ts/src/jdom/utils';
+import ErrorIcon from "@material-ui/icons/Error"
+import { JSONTryParse } from "../../jacdac-ts/src/jdom/utils"
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         device: {
-            marginRight: theme.spacing(1)
+            marginRight: theme.spacing(1),
         },
         item: {
             marginBottom: 0,
@@ -21,42 +28,50 @@ const useStyles = makeStyles((theme: Theme) =>
             borderBottom: "1px solid #ddd",
         },
         debug: {
-            color: "gray"
+            color: "gray",
         },
-        log: {
-        },
+        log: {},
         warn: {
-            background: "#ffffd5"
+            background: "#ffffd5",
         },
-        error: {
-        }
+        error: {},
     })
-);
+)
 
-export default function ConsoleListItem(props: {
-    packet: Packet
-}) {
-    const { packet } = props;
+export default function ConsoleListItem(props: { packet: Packet }) {
+    const { packet } = props
     const classes = useStyles()
-    const decoded = decodePacketData(packet);
-    const severity = decoded.info.identifier;
-    const decodedText = decoded?.decoded[0];
+    const decoded = decodePacketData(packet)
+    const severity = decoded.info.identifier
+    const decodedText = decoded?.decoded[0]
     const text = JSONTryParse(decodedText?.humanValue) || "???"
 
     function severityClass() {
         switch (severity) {
-            case 0x80: return classes.debug;
-            case 0x82: return classes.warn;
-            case 0x83: return classes.error;
-            default: return classes.log
+            case 0x80:
+                return classes.debug
+            case 0x82:
+                return classes.warn
+            case 0x83:
+                return classes.error
+            default:
+                return classes.log
         }
     }
 
-    return <ListItem dense={true} className={clsx(classes.item, severityClass())}>
-        {severity >= 0x82 && <ListItemIcon>
-            {severity == 0x82 ? <WarningIcon /> : <ErrorIcon />}
-        </ListItemIcon>}
-        <Chip className={classes.device} size="small" label={packet?.device?.shortId || "?"} />
-        <Typography variant="body2">{text}</Typography>
-    </ListItem>
+    return (
+        <ListItem dense={true} className={clsx(classes.item, severityClass())}>
+            {severity >= 0x82 && (
+                <ListItemIcon>
+                    {severity == 0x82 ? <WarningIcon /> : <ErrorIcon />}
+                </ListItemIcon>
+            )}
+            <Chip
+                className={classes.device}
+                size="small"
+                label={packet?.device?.shortId || "?"}
+            />
+            <Typography variant="body2">{text}</Typography>
+        </ListItem>
+    )
 }
--- src/components/tools/PacketView.tsx
+++ src/components/tools/PacketView.tsx
@@ -1,74 +1,82 @@
-import React, { useContext } from 'react';
-import { makeStyles, createStyles } from '@material-ui/core';
-import PacketListItem from '../PacketListItem';
+import React, { useContext } from "react"
+import { makeStyles, createStyles } from "@material-ui/core"
+import PacketListItem from "../PacketListItem"
 // tslint:disable-next-line: no-submodule-imports match-default-export-name
-import PacketsContext from '../PacketsContext';
+import PacketsContext from "../PacketsContext"
 // tslint:disable-next-line: no-submodule-imports match-default-export-name
-import { FixedSizeList, ListChildComponentProps } from 'react-window';
-import AutoSizer from 'react-virtualized-auto-sizer'
-import PacketFilter from '../PacketFilter';
-import { TracePacketProps } from '../../../jacdac-ts/src/jdom/trace/traceview';
+import { FixedSizeList, ListChildComponentProps } from "react-window"
+import AutoSizer from "react-virtualized-auto-sizer"
+import PacketFilter from "../PacketFilter"
+import { TracePacketProps } from "../../../jacdac-ts/src/jdom/trace/traceview"
 
 const useStyles = makeStyles(() =>
     createStyles({
         items: {
-            flex: 1
-        }
+            flex: 1,
+        },
     })
-);
+)
 
 interface VirtualListData {
-    packets: TracePacketProps[];
-    showTime: boolean;
+    packets: TracePacketProps[]
+    showTime: boolean
 }
 
-const VirtualPacketItem = (props: { data: VirtualListData }
-    & ListChildComponentProps) => {
-    const { style, index, data } = props;
+const VirtualPacketItem = (
+    props: { data: VirtualListData } & ListChildComponentProps
+) => {
+    const { style, index, data } = props
     const { packets, showTime } = data
-    const packet = packets[index];
+    const packet = packets[index]
 
-    if (!packet)
-        return <div key={""} style={style}></div>
+    if (!packet) return <div key={""} style={style}></div>
 
-    return <div key={packet.key} style={style}>
-        <PacketListItem
-            packet={packet.packet}
-            count={packet.count}
-            showTime={showTime} />
-    </div>
+    return (
+        <div key={packet.key} style={style}>
+            <PacketListItem
+                packet={packet.packet}
+                count={packet.count}
+                showTime={showTime}
+            />
+        </div>
+    )
 }
 
 function VirtualPacketList(props: { showTime?: boolean }) {
-    const { showTime } = props;
+    const { showTime } = props
     const classes = useStyles()
     const { packets } = useContext(PacketsContext)
     const itemData: VirtualListData = {
         showTime,
-        packets
+        packets,
     }
-    return <AutoSizer className={classes.items}>
-        {({ height, width }) => (
-            <FixedSizeList
-                itemCount={packets.length}
-                itemSize={54}
-                height={height}
-                width={width}
-                itemData={itemData}>
-                {VirtualPacketItem}
-            </FixedSizeList>
-        )}
-    </AutoSizer>
+    return (
+        <AutoSizer className={classes.items}>
+            {({ height, width }) => (
+                <FixedSizeList
+                    itemCount={packets.length}
+                    itemSize={54}
+                    height={height}
+                    width={width}
+                    itemData={itemData}
+                >
+                    {VirtualPacketItem}
+                </FixedSizeList>
+            )}
+        </AutoSizer>
+    )
 }
 
 export default function PacketView(props: {
-    serviceClass?: number,
+    serviceClass?: number
     showTime?: boolean
 }) {
     const { showTime } = props
 
-    return (<>
-        <PacketFilter />
-        <VirtualPacketList showTime={showTime} />
-    </>)
+    return (
+        <>
+            <PacketFilter />
+            <VirtualPacketList showTime={showTime} />
+        </>
+    )
 }
