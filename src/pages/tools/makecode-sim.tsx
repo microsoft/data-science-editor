6bf6e404b4d867b96cc170d4c862b830b0b4fab7
pelikhan <jhalleux@microsoft.com>
2021-09-03 13:14:08 -0700

    refactor makecode button

--- src/pages/tools/makecode-sim.tsx
+++ src/pages/tools/makecode-sim.tsx
@@ -1,19 +1,12 @@
 import React, { useContext, useEffect } from "react"
-import {
-    Box,
-    Button,
-    createTheme,
-    responsiveFontSizes,
-} from "@material-ui/core"
+import { createTheme, responsiveFontSizes } from "@material-ui/core"
 import ThemedLayout from "../../components/ui/ThemedLayout"
 import JDDevice from "../../../jacdac-ts/src/jdom/device"
 import { isReading, isValueOrIntensity } from "../../../jacdac-ts/src/jdom/spec"
 import { strcmp } from "../../../jacdac-ts/src/jdom/utils"
-import MakeCodeIcon from "../../components/icons/MakeCodeIcon"
 import Helmet from "react-helmet"
 import Dashboard from "../../components/dashboard/Dashboard"
 import JacdacContext, { JacdacContextProps } from "../../jacdac/Context"
-import useChange from "../../jacdac/useChange"
 import DarkModeContext from "../../components/ui/DarkModeContext"
 import IFrameBridgeClient from "../../components/makecode/iframebridgeclient"
 
@@ -52,33 +45,15 @@ function Carousel() {
     ] as IFrameBridgeClient
     const deviceFilter = iframeBridge.deviceFilter.bind(iframeBridge)
     const serviceFilter = iframeBridge.serviceFilter.bind(iframeBridge)
-    const extensions = useChange(iframeBridge, _ => _?.candidateExtensions)
-    const handleAdd = () => iframeBridge?.postAddExtensions()
 
     return (
-        <>
-            <Dashboard
-                showHeader={false}
-                deviceSort={deviceSort}
-                deviceFilter={deviceFilter}
-                serviceFilter={serviceFilter}
-                showStartSimulators={false}
-            />
-            {!!extensions?.length && (
-                <Box m={1}>
-                    <Button
-                        size="medium"
-                        color="primary"
-                        variant="contained"
-                        startIcon={<MakeCodeIcon />}
-                        onClick={handleAdd}
-                        aria-label={"Add blocks"}
-                    >
-                        Add blocks
-                    </Button>
-                </Box>
-            )}
-        </>
+        <Dashboard
+            showHeader={false}
+            deviceSort={deviceSort}
+            deviceFilter={deviceFilter}
+            serviceFilter={serviceFilter}
+            showStartSimulators={false}
+        />
     )
 }
 
