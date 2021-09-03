574822bc344930434bc544a1baa0271f93224480
rwilliams-msr <t-rwilliams@microsoft.com>
2021-09-03 16:19:55 -0400

    Remove average pooling layers, not implemented yet

--- src/components/model-editor/modelblockdsl.ts
+++ src/components/model-editor/modelblockdsl.ts
@@ -406,10 +406,10 @@ export class ModelBlockDomainSpecificLanguage
                         kind: "block",
                         blockxml: `<block type="model_block_maxpool1d_layer"><field name="EXPAND_BUTTON">{"parametersVisible":false,"percentSize":0,"percentParams":0,"runTimeInMs":0,"outputShape":[0,0],"poolSize":2,"strideSize":1}</field></block>`,
                     },
-                    {
+                    /*{
                         kind: "block",
                         blockxml: `<block type="model_block_avgpool1d_layer"><field name="EXPAND_BUTTON">{"parametersVisible":false,"percentSize":0,"percentParams":0,"runTimeInMs":0,"outputShape":[0,0],"poolSize":2,"strideSize":1}</field></block>`,
-                    },
+                    },*/
                     {
                         kind: "block",
                         blockxml: `<block type="model_block_conv2d_layer"><field name="EXPAND_BUTTON">{"parametersVisible":false,"percentSize":0,"percentParams":0,"runTimeInMs":0,"outputShape":[0,0,0],"numFilters":16,"kernelSize":2,"strideSize":1,"activation":"relu"}</field></block>`,
@@ -418,10 +418,10 @@ export class ModelBlockDomainSpecificLanguage
                         kind: "block",
                         blockxml: `<block type="model_block_maxpool2d_layer"><field name="EXPAND_BUTTON">{"parametersVisible":false,"percentSize":0,"percentParams":0,"runTimeInMs":0,"outputShape":[0,0,0],"poolSize":2,"strideSize":1}</field></block>`,
                     },
-                    {
+                    /*{
                         kind: "block",
                         blockxml: `<block type="model_block_avgpool2d_layer"><field name="EXPAND_BUTTON">{"parametersVisible":false,"percentSize":0,"percentParams":0,"runTimeInMs":0,"outputShape":[0,0,0],"poolSize":2,"strideSize":1}</field></block>`,
-                    },
+                    },*/
                     {
                         kind: "block",
                         blockxml: `<block type="model_block_dropout_layer"><field name="EXPAND_BUTTON">{"parametersVisible":false,"percentSize":0,"percentParams":0,"runTimeInMs":0,"outputShape":[0,0],"rate":0.1}</field></block>`,
@@ -433,10 +433,6 @@ export class ModelBlockDomainSpecificLanguage
                     {
                         kind: "block",
                         blockxml: `<block type="model_block_dense_layer"><field name="EXPAND_BUTTON">{"parametersVisible":false,"percentSize":0,"percentParams":0,"runTimeInMs":0,"outputShape":[0],"numUnits":4,"activation":"relu"}</field></block>`,
-                    },
-                    {
-                        kind: "label",
-                        text: "Visualizations",
                     },
                 ],
             })]
