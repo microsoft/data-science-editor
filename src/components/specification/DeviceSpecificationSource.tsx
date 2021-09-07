37171937d1c73dd18c19cdfa18913fc3a33fcc25
pelikhan <jhalleux@microsoft.com>
2021-09-07 14:18:36 -0700

    simplify source displays

--- src/components/specification/DeviceSpecificationSource.tsx
+++ src/components/specification/DeviceSpecificationSource.tsx
@@ -3,26 +3,10 @@ import React, { useState } from "react"
 import Tabs from "@material-ui/core/Tabs"
 // tslint:disable-next-line: no-submodule-imports
 import Tab from "@material-ui/core/Tab"
-import { Paper, createStyles, makeStyles, Theme } from "@material-ui/core"
+import { Paper } from "@material-ui/core"
 import TabPanel from "../ui/TabPanel"
 import Snippet from "../ui/Snippet"
 import DeviceSpecification from "./DeviceSpecification"
-
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        root: {
-            flexGrow: 1,
-            backgroundColor: theme.palette.background.paper,
-            marginBottom: theme.spacing(1),
-        },
-        pre: {
-            margin: "0",
-            padding: "0",
-            backgroundColor: "transparent",
-            whiteSpec: "pre-wrap",
-        },
-    })
-)
 
 export default function DeviceSpecificationSource(props: {
     deviceSpecification?: jdspec.DeviceSpec
@@ -30,7 +14,6 @@ export default function DeviceSpecificationSource(props: {
     showJSON?: boolean
 }) {
     const { deviceSpecification, showSpecification, showJSON } = props
-    const classes = useStyles()
     const [tab, setTab] = useState(0)
     const spec = deviceSpecification
 
@@ -43,36 +26,31 @@ export default function DeviceSpecificationSource(props: {
 
     let index = 0
     return (
-        <div className={classes.root}>
-            <Paper square>
-                <Tabs
-                    value={tab}
-                    onChange={handleTabChange}
-                    aria-label="View specification formats"
-                >
-                    {[
-                        showSpecification && "Specification",
-                        showJSON && "JSON",
-                    ]
-                        .filter(n => !!n)
-                        .map(n => (
-                            <Tab key={n} label={n} />
-                        ))}
-                </Tabs>
-                {showSpecification && (
-                    <TabPanel key="spec" value={tab} index={index++}>
-                        <DeviceSpecification device={spec} />
-                    </TabPanel>
-                )}
-                {showJSON && (
-                    <TabPanel key={`convjson`} value={tab} index={index++}>
-                        <Snippet
-                            value={JSON.stringify(spec, null, 2)}
-                            mode={"json"}
-                        />
-                    </TabPanel>
-                )}
-            </Paper>
-        </div>
+        <Paper square>
+            <Tabs
+                value={tab}
+                onChange={handleTabChange}
+                aria-label="View specification formats"
+            >
+                {[showSpecification && "Specification", showJSON && "JSON"]
+                    .filter(n => !!n)
+                    .map(n => (
+                        <Tab key={n} label={n} />
+                    ))}
+            </Tabs>
+            {showSpecification && (
+                <TabPanel key="spec" value={tab} index={index++}>
+                    <DeviceSpecification device={spec} />
+                </TabPanel>
+            )}
+            {showJSON && (
+                <TabPanel key={`convjson`} value={tab} index={index++}>
+                    <Snippet
+                        value={JSON.stringify(spec, null, 2)}
+                        mode={"json"}
+                    />
+                </TabPanel>
+            )}
+        </Paper>
     )
 }
--- src/components/specification/ServiceSpecificationSource.tsx
+++ src/components/specification/ServiceSpecificationSource.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react"
 import { Tabs, Tab } from "@material-ui/core"
 import { serviceSpecificationFromClassIdentifier } from "../../../jacdac-ts/src/jdom/spec"
-import { Paper, createStyles, makeStyles, Theme } from "@material-ui/core"
+import { Paper } from "@material-ui/core"
 import TabPanel from "../ui/TabPanel"
 import Snippet from "../ui/Snippet"
 import { converters } from "../../../jacdac-ts/jacdac-spec/spectool/jdspec"
@@ -9,29 +9,12 @@ import ServiceSpecification from "./ServiceSpecification"
 import { serviceSpecificationToServiceTwinSpecification } from "../../../jacdac-ts/src/azure-iot/devicetwin"
 import { withPrefix } from "gatsby"
 
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        root: {
-            flexGrow: 1,
-            backgroundColor: theme.palette.background.paper,
-            marginBottom: theme.spacing(1),
-        },
-        pre: {
-            margin: "0",
-            padding: "0",
-            backgroundColor: "transparent",
-            whiteSpec: "pre-wrap",
-        },
-    })
-)
-
 export default function ServiceSpecificationSource(props: {
     classIdentifier?: number
     serviceSpecification?: jdspec.ServiceSpec
     showSpecification?: boolean
 }) {
     const { classIdentifier, serviceSpecification, showSpecification } = props
-    const classes = useStyles()
     const [tab, setTab] = useState(0)
     const spec =
         serviceSpecification ||
@@ -48,56 +31,54 @@ export default function ServiceSpecificationSource(props: {
 
     let index = 0
     return (
-        <div className={classes.root}>
-            <Paper square>
-                <Tabs
-                    value={tab}
-                    onChange={handleTabChange}
-                    aria-label="View specification formats"
-                >
-                    {[
-                        showSpecification && "Specification",
-                        "MakeCode",
-                        "TypeScript",
-                        "C",
-                        "JSON",
-                        showDeviceTwin && "Twin",
-                    ]
-                        .filter(n => !!n)
-                        .map((n, i) => (
-                            <Tab key={n} label={n} />
-                        ))}
-                </Tabs>
-                {showSpecification && (
-                    <TabPanel key="spec" value={tab} index={index++}>
-                        <ServiceSpecification service={spec} />
-                    </TabPanel>
-                )}
-                {["sts", "ts", "c", "json"].map(lang => (
-                    <TabPanel key={`conv${lang}`} value={tab} index={index++}>
-                        <Snippet value={() => convs[lang](spec)} mode={lang} />
-                    </TabPanel>
-                ))}
-                {showDeviceTwin && (
-                    <TabPanel key="devicetwin" value={tab} index={index++}>
-                        <Snippet
-                            mode="json"
-                            url={withPrefix(
-                                `/services/twin/x${spec.classIdentifier.toString(
-                                    16
-                                )}.json`
-                            )}
-                            value={JSON.stringify(
-                                serviceSpecificationToServiceTwinSpecification(
-                                    spec
-                                ),
-                                null,
-                                2
-                            )}
-                        />
-                    </TabPanel>
-                )}
-            </Paper>
-        </div>
+        <Paper square>
+            <Tabs
+                value={tab}
+                onChange={handleTabChange}
+                aria-label="View specification formats"
+            >
+                {[
+                    showSpecification && "Specification",
+                    "MakeCode",
+                    "TypeScript",
+                    "C",
+                    "JSON",
+                    showDeviceTwin && "Twin",
+                ]
+                    .filter(n => !!n)
+                    .map(n => (
+                        <Tab key={n} label={n} />
+                    ))}
+            </Tabs>
+            {showSpecification && (
+                <TabPanel key="spec" value={tab} index={index++}>
+                    <ServiceSpecification service={spec} />
+                </TabPanel>
+            )}
+            {["sts", "ts", "c", "json"].map(lang => (
+                <TabPanel key={`conv${lang}`} value={tab} index={index++}>
+                    <Snippet value={() => convs[lang](spec)} mode={lang} />
+                </TabPanel>
+            ))}
+            {showDeviceTwin && (
+                <TabPanel key="devicetwin" value={tab} index={index++}>
+                    <Snippet
+                        mode="json"
+                        url={withPrefix(
+                            `/services/twin/x${spec.classIdentifier.toString(
+                                16
+                            )}.json`
+                        )}
+                        value={JSON.stringify(
+                            serviceSpecificationToServiceTwinSpecification(
+                                spec
+                            ),
+                            null,
+                            2
+                        )}
+                    />
+                </TabPanel>
+            )}
+        </Paper>
     )
 }
