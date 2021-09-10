931a28ab4791038963a4d66b2af72c673263eedc
pelikhan <jhalleux@microsoft.com>
2021-09-10 15:58:20 -0700

    don't point to beta editor

--- src/components/makecode/MakeCodeSnippetContext.tsx
+++ src/components/makecode/MakeCodeSnippetContext.tsx
@@ -1,47 +1,59 @@
-import React, { createContext } from "react";
+import React, { createContext } from "react"
 import useLocalStorage from "../hooks/useLocalStorage"
 
 const editors = {
-    arcade: "https://arcade.makecode.com/beta/",
-    microbit: "https://makecode.microbit.org/beta/",
-    maker: "https://maker.makecode.com/"
+    arcade: "https://arcade.makecode.com/",
+    microbit: "https://makecode.microbit.org/",
+    maker: "https://maker.makecode.com/",
 }
 export interface MakeCodeSnippetContextProps {
-    target: string, setTarget: (t: string) => void,
-    editor: string, setEditor: (t: string) => void,
-    rendererUrl: string,
-    simUrl: string,
+    target: string
+    setTarget: (t: string) => void
+    editor: string
+    setEditor: (t: string) => void
+    rendererUrl: string
+    simUrl: string
 }
 
 const MakeCodeSnippetContext = createContext<MakeCodeSnippetContextProps>({
     target: undefined,
-    setTarget: (t) => { },
+    setTarget: t => {},
     editor: undefined,
-    setEditor: (t) => { },
+    setEditor: t => {},
     rendererUrl: undefined,
     simUrl: undefined,
-});
-MakeCodeSnippetContext.displayName = "MakeCode";
+})
+MakeCodeSnippetContext.displayName = "MakeCode"
 
-export default MakeCodeSnippetContext;
+export default MakeCodeSnippetContext
 
 export function MakeCodeSnippetProvider(props: { children }) {
-    const [target, setTarget] = useLocalStorage("mkcd:editor", "microbit");
-    const [editor, setEditor] = useLocalStorage("mdcd:editor", "blocks");
-    const { children } = props;
+    const [target, setTarget] = useLocalStorage("mkcd:editor", "microbit")
+    const [editor, setEditor] = useLocalStorage("mdcd:editor", "blocks")
+    const { children } = props
 
-    const useLocalhost = typeof window !== "undefined" && /localhostmakecode=1/.test(window.location.search);
-    const rendererUrl =
-        useLocalhost ? "http://localhost:3232/--docs"
-            : ((editors[target] || editors["microbit"]) + "---docs")
-    const simUrl =
-        useLocalhost ? "http://localhost:3232/--run"
-            : ((editors[target] || editors["microbit"]) + "---run")
+    const useLocalhost =
+        typeof window !== "undefined" &&
+        /localhostmakecode=1/.test(window.location.search)
+    const rendererUrl = useLocalhost
+        ? "http://localhost:3232/--docs"
+        : (editors[target] || editors["microbit"]) + "---docs"
+    const simUrl = useLocalhost
+        ? "http://localhost:3232/--run"
+        : (editors[target] || editors["microbit"]) + "---run"
 
-    return <MakeCodeSnippetContext.Provider value={{
-        target, setTarget, editor, setEditor,
-        rendererUrl, simUrl
-    }}>
-        {children}
-    </MakeCodeSnippetContext.Provider>;
+    return (
+        <MakeCodeSnippetContext.Provider
+            value={{
+                target,
+                setTarget,
+                editor,
+                setEditor,
+                rendererUrl,
+                simUrl,
+            }}
+        >
+            {children}
+        </MakeCodeSnippetContext.Provider>
+    )
 }
