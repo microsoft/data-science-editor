5c13870da99d4e856d15796d9b291821b04dc64d
pelikhan <jhalleux@microsoft.com>
2021-09-10 13:02:34 -0700

    fix link

--- src/components/shell/ToolsDrawer.tsx
+++ src/components/shell/ToolsDrawer.tsx
@@ -162,7 +162,7 @@ export default function ToolsDrawer() {
         },
         {
             text: "MakeCode",
-            href: "/clients/makecode/",
+            to: "/clients/makecode/",
             icon: <MakeCodeIcon />,
         },
         /*
