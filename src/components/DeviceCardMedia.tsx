6036b19b89427fcc4ce5edd64462c5cf4c38017a
pelikhan <jhalleux@microsoft.com>
2021-09-15 16:01:13 -0700

    formatting

--- src/components/DeviceCardMedia.tsx
+++ src/components/DeviceCardMedia.tsx
@@ -1,16 +1,15 @@
-import React from "react";
-import JDDevice from "../../jacdac-ts/src/jdom/device";
-import useDeviceSpecification from "../jacdac/useDeviceSpecification";
-import useDeviceImage from "./devices/useDeviceImage";
-import CardMediaWithSkeleton from "./ui/CardMediaWithSkeleton";
+import React from "react"
+import JDDevice from "../../jacdac-ts/src/jdom/device"
+import useDeviceSpecification from "../jacdac/useDeviceSpecification"
+import useDeviceImage from "./devices/useDeviceImage"
+import CardMediaWithSkeleton from "./ui/CardMediaWithSkeleton"
 
 export default function DeviceCardMedia(props: { device: JDDevice }) {
-    const { device } = props;
+    const { device } = props
     const specification = useDeviceSpecification(device)
     const imageUrl = useDeviceImage(specification)
 
-    return <CardMediaWithSkeleton
-        image={imageUrl}
-        title={specification?.name}
-    />
+    return (
+        <CardMediaWithSkeleton image={imageUrl} title={specification?.name} />
+    )
 }
