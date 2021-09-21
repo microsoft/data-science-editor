598917af5268de97990dbeb00d4ea0373293b046
pelikhan <jhalleux@microsoft.com>
2021-09-21 09:19:35 -0700

    updating kit page

--- src/components/home/Kit.tsx
+++ src/components/home/Kit.tsx
@@ -1,6 +1,8 @@
 import { Grid } from "@material-ui/core"
 import { StaticImage } from "gatsby-plugin-image"
 import React from "react"
+import CarouselGrid from "./CarouselGrid"
+import FeatureItem from "./FeatureItem"
 import SplitGrid from "./SplitGrid"
 
 export default function Kit() {
@@ -14,44 +16,59 @@ export default function Kit() {
         >
             <SplitGrid
                 title="Hardware Kit"
-                subtitle3="Hardware Module Kit"
+                subtitle3="Start experimenting with Jacdac and the Hardware module kit."
                 imageColumns={6}
                 image={
                     <StaticImage
-                        src="./kittop.jpg"
+                        src="./kit/topfront.jpg"
                         alt="Kit cardboard view from top"
                     />
                 }
             />
-
-            <SplitGrid
-                right={true}
-                subtitle="Explore"
-                subtitle3="Use the dashboard to interact with modules in the browser."
-                imageColumns={6}
-                image={
-                    <StaticImage
-                        src="./dashboardkit.png"
-                        alt="Jacdac devices connected to a computer with a dashboard"
+            <CarouselGrid>
+                <Grid item xs={12} sm={4}>
+                    <FeatureItem
+                        startImage={
+                            <StaticImage
+                                src="./kit/opentopside.jpg"
+                                alt="Hardware kit box opened with contents on a table"
+                            />
+                        }
+                        subtitle2="Unbox"
+                        description="Use the dashboard to interact with modules in the browser."
+                        buttonText="Modules"
+                        buttonUrl="/hardware/kit/modules"
                     />
-                }
-                buttonText="Dashboard"
-                buttonUrl="/dashboard"
-            />
-
-            <SplitGrid
-                right={false}
-                subtitle="Build"
-                description="Code Jacdac into your micro:bit V2, Arcade or Maker board."
-                buttonText="MakeCode"
-                buttonUrl="/clients/makecode"
-                image={
-                    <StaticImage
-                        src="./makecode.png"
-                        alt="Block code to swipe a servo"
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <FeatureItem
+                        startImage={
+                            <StaticImage
+                                src="./dashboardkit.png"
+                                alt="Jacdac devices connected to a computer with a dashboard"
+                            />
+                        }
+                        subtitle2="Explore"
+                        description="Use the dashboard to interact with modules in the browser."
+                        buttonText="Dashboard"
+                        buttonUrl="/dashboard"
                     />
-                }
-            />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <FeatureItem
+                        startImage={
+                            <StaticImage
+                                src="./makecode.png"
+                                alt="Block code to swipe a servo"
+                            />
+                        }
+                        subtitle="Build"
+                        description="Code Jacdac into your micro:bit V2, Arcade or Maker board."
+                        buttonText="MakeCode"
+                        buttonUrl="/clients/makecode"
+                    />
+                </Grid>
+            </CarouselGrid>
         </Grid>
     )
 }
