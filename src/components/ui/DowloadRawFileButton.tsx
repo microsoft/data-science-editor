81309515232fe340df9c54d413b4718f7969768e
pelikhan <jhalleux@microsoft.com>
2021-09-24 11:27:16 -0700

    fix oob download button

---
+++ src/components/ui/DowloadRawFileButton.tsx
@@ -0,0 +1,29 @@
+import { ButtonProps } from "@material-ui/core"
+import { Button } from "gatsby-material-ui-components"
+import React, { ReactNode, useContext } from "react"
+import JacdacContext, { JacdacContextProps } from "../../jacdac/Context"
+import ServiceManagerContext from "../ServiceManagerContext"
+
+export default function DowloadRawFileButton(
+    props: {
+        url: string
+        name: string
+        children: ReactNode
+        disconnect?: boolean
+    } & ButtonProps
+) {
+    const { url, name, children, disconnect, ...rest } = props
+    const { bus } = useContext<JacdacContextProps>(JacdacContext)
+    const { fileStorage } = useContext(ServiceManagerContext)
+    const handleClick = async () => {
+        if (disconnect) await bus.disconnect()
+        const req = await fetch(url)
+        const text = await req.text()
+        fileStorage.saveText(name, text)
+    }
+    return (
+        <Button {...rest} onClick={handleClick}>
+            {children}
+        </Button>
+    )
+}
