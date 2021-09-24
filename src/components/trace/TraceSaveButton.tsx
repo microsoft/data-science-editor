7c0094c1294302f6bd597569e3e2dce98b8f0b50
pelikhan <jhalleux@microsoft.com>
2021-09-24 11:00:47 -0700

    updated trace format to dump firmware info

--- src/components/trace/TraceSaveButton.tsx
+++ src/components/trace/TraceSaveButton.tsx
@@ -4,13 +4,31 @@ import SaveIcon from "@material-ui/icons/Save"
 import ServiceManagerContext from "../ServiceManagerContext"
 import PacketsContext from "../PacketsContext"
 import IconButtonWithTooltip from "../ui/IconButtonWithTooltip"
+import JacdacContext, { JacdacContextProps } from "../../jacdac/Context"
 
 export default function TraceSaveButton() {
+    const { bus } = useContext<JacdacContextProps>(JacdacContext)
     const { replayTrace, view, recording, tracing } = useContext(PacketsContext)
     const { fileStorage } = useContext(ServiceManagerContext)
     const saveTrace = () => {
+        const busText = bus.describe()
         const savedTrace = replayTrace || view.trace
-        const text = savedTrace.serializeToText()
+        const traceText = savedTrace.serializeToText()
+        const text = `# Jacdac Trace
+
+## devices
+
+\`\`\`yaml
+${busText}
+\`\`\`
+
+## packets
+
+\`\`\`
+${traceText}
+\`\`\`
+`
+        console.log({ busText, traceText, text })
         fileStorage.saveText("trace.jd.txt", text)
     }
     return (
