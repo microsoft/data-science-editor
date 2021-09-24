46c930bf352f645d2a2c59aae812351a45c7659f
pelikhan <jhalleux@microsoft.com>
2021-09-23 22:35:18 -0700

    hide breadcrumbs in tools

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -162,6 +162,7 @@ function LayoutWithContext(props: LayoutProps) {
     const { element, props: pageProps } = props
     const { pageContext, path, location } = pageProps
     const { frontmatter } = pageContext || {}
+    const tools = /tools\//.test(path)
     const makeCodeTool = /tools\/makecode-/.test(path)
     const fullWidthTools =
         /^\/editors\/\w+\/$/.test(path) ||
@@ -173,7 +174,7 @@ function LayoutWithContext(props: LayoutProps) {
     } = frontmatter || {
         hideMainMenu: makeCodeTool,
         hideUnderConstruction: makeCodeTool || fullWidthTools,
-        hideBreadcrumbs: fullWidthTools,
+        hideBreadcrumbs: tools,
     }
 
     const isDataEditor = /^\/editors\/data/.test(path)
