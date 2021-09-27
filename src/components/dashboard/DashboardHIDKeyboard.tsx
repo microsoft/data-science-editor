411000e0e19eb341b7c8b2fde08d5f87b464a6d8
pelikhan <jhalleux@microsoft.com>
2021-09-27 12:00:17 -0700

    trigger keyboard keys with button

--- src/components/dashboard/DashboardHIDKeyboard.tsx
+++ src/components/dashboard/DashboardHIDKeyboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useCallback, useContext, useEffect, useState } from "react"
 import { Grid } from "@material-ui/core"
 import {
     HidKeyboardAction,
@@ -11,10 +11,20 @@ import CmdButton from "../CmdButton"
 import ClearIcon from "@material-ui/icons/Clear"
 import KeyboardKeyInput from "../ui/KeyboardKeyInput"
 import KeyboardIcon from "@material-ui/icons/Keyboard"
+import SelectEvent from "../select/SelectEvent"
+import useEvents from "../hooks/useEvents"
+import useChange from "../../jacdac/useChange"
+import { EVENT } from "../../../jacdac-ts/src/jdom/constants"
+import JDEvent from "../../../jacdac-ts/src/jdom/event"
+import JacdacContext, { JacdacContextProps } from "../../jacdac/Context"
+
 export default function DashboardHIDKeyboard(props: DashboardServiceProps) {
     const { service } = props
+    const { bus } = useContext<JacdacContextProps>(JacdacContext)
     const [selector, setSelector] = useState(0)
     const [modifiers, setModifiers] = useState(HidKeyboardModifiers.None)
+    const [triggerEventId, setTriggerEventId] = useState<string>("")
+    const events = useEvents({ ignoreChange: true })
 
     const handleKeyChange = (
         newSelector: number,
@@ -24,20 +34,26 @@ export default function DashboardHIDKeyboard(props: DashboardServiceProps) {
         setModifiers(newModifiers)
     }
 
-    const handleClear = async () => {
-        setSelector(0)
-        setModifiers(HidKeyboardModifiers.None)
-        await service.sendCmdAsync(HidKeyboardCmd.Clear)
-    }
-
-    const handleClick = async () => {
+    const handleClick = useCallback(async () => {
         const unpacked: [[number, HidKeyboardModifiers, HidKeyboardAction][]] =
             [[[selector, modifiers, HidKeyboardAction.Press]]]
         const data = jdpack("r: u16 u8 u8", unpacked)
         await service.sendCmdAsync(HidKeyboardCmd.Key, data)
-    }
+    }, [selector, modifiers, service])
+
+    useChange(
+        bus,
+        () => {
+            const triggerEvent = bus.node(triggerEventId) as JDEvent
+            console.log({ triggerEventId, triggerEvent, selector, modifiers })
+            const un = triggerEvent?.subscribe(EVENT, handleClick)
+            return () => un?.()
+        },
+        [triggerEventId, handleClick]
+    )
 
     const disabled = !selector && !modifiers
+    const handleTriggerChange = (eventId: string) => setTriggerEventId(eventId)
 
     return (
         <Grid container direction="column" spacing={1}>
@@ -49,20 +65,28 @@ export default function DashboardHIDKeyboard(props: DashboardServiceProps) {
                 />
             </Grid>
             <Grid item xs={12}>
-                <CmdButton
-                    title="send keys"
-                    disabled={disabled}
-                    onClick={handleClick}
-                    icon={<KeyboardIcon />}
-                >
-                    Send keys
-                </CmdButton>
-                <CmdButton
-                    title="clear keys"
-                    disabled={disabled}
-                    onClick={handleClear}
-                    icon={<ClearIcon />}
-                />
+                <Grid container spacing={1}>
+                    <Grid item>
+                        <CmdButton
+                            variant="outlined"
+                            title="send keys"
+                            disabled={disabled}
+                            onClick={handleClick}
+                            icon={<KeyboardIcon />}
+                        >
+                            Send keys
+                        </CmdButton>
+                    </Grid>
+                    <Grid item>
+                        <SelectEvent
+                            events={events}
+                            eventId={triggerEventId}
+                            onChange={handleTriggerChange}
+                            label={"Choose Send Key event"}
+                            friendlyName={true}
+                        />
+                    </Grid>
+                </Grid>
             </Grid>
         </Grid>
     )
