d49c73f1ebc2ad5d08b55b03676bc5e54e421f51
pelikhan <jhalleux@microsoft.com>
2021-09-26 17:45:12 -0700

    better stretch sizing of 3d models

--- src/components/home/models/ModelViewer.js
+++ src/components/home/models/ModelViewer.js
@@ -7,7 +7,13 @@ export default function ModelViewer(props) {
     const { children } = props
     const ref = useRef()
     return (
-        <div style={{ position: "relative", width: 640, height: 480 }}>
+        <div
+            style={{
+                position: "relative",
+                width: "clamp(640, 100vw - 1rem)",
+                height: "clamp(480, 100vh - 1rem)",
+            }}
+        >
             <Canvas shadows dpr={[1, 2]} camera={{ fov: 50 }}>
                 <Suspense fallback={null}>
                     <Stage
