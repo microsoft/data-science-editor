4b5837c11a4f8ceb9927714b74a6df58a5d6db6c
Tom Ball <you@example.com>
2021-09-27 15:14:50 -0700

    create project directory

---
+++ src/components/makecode/MakeCodeProjects.tsx
@@ -0,0 +1,54 @@
+import { List, ListItem, ListItemText } from "@material-ui/core"
+import { graphql, Link, useStaticQuery } from "gatsby"
+import React from "react"
+
+export default function MakeCodeProjects() {
+    const query = useStaticQuery<{
+        allMdx: {
+            edges: {
+                node: {
+                    fields: {
+                        slug: string
+                    }
+                    frontmatter: {
+                        title?: string
+                    }
+                }
+            }[]
+        }
+    }>(graphql`
+        {
+            allMdx(
+                filter: {
+                    fields: { slug: { glob: "/clients/makecode/projects/*" } }
+                }
+            ) {
+                edges {
+                    node {
+                        id
+                        fields {
+                            slug
+                        }
+                        frontmatter {
+                            title
+                        }
+                    }
+                }
+            }
+        }
+    `)
+    const nodes = query.allMdx.edges
+        .map(edge => edge.node)
+        .sort((l, r) => l.fields.slug.localeCompare(r.fields.slug))
+    return (
+        <List>
+            {nodes?.map(({ fields, frontmatter }) => (
+                <Link to={fields.slug} key={fields.slug}>
+                    <ListItem>
+                        <ListItemText primary={frontmatter.title} />
+                    </ListItem>
+                </Link>
+            ))}
+        </List>
+    )
+}
