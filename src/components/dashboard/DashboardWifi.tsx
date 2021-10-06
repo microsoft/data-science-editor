6caec2a0f432408e6de438eb1c1e2619cf4fd0d8
pelikhan <jhalleux@microsoft.com>
2021-10-06 16:31:00 -0700

    more wifi dashboard

--- src/components/dashboard/DashboardWifi.tsx
+++ src/components/dashboard/DashboardWifi.tsx
@@ -274,7 +274,7 @@ export default function DashboardWifi(props: DashboardServiceProps) {
                                 key={nssid}
                                 priority={priority}
                                 flags={flags}
-                                ssid={ssid}
+                                ssid={nssid}
                             />
                         ))}
                     </List>
