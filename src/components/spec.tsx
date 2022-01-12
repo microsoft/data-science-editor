b37eebaadcb8a5835d82191654b7f1a416b33aaa
pelikhan <jhalleux@microsoft.com>
2022-01-12 10:25:14 -0800

    showtitle

--- src/components/spec.tsx
+++ src/components/spec.tsx
@@ -1,12 +1,33 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { Helmet } from "react-helmet"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default function PageTemplate(props: { data: { mdx: { body: any } } }) {
+export default function PageTemplate(props: {
+    data: {
+        mdx: {
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            body: any
+            id: string
+            frontmatter?: { title?: string; hideToc?: boolean }
+        }
+    }
+}) {
     const { data } = props
     const { mdx } = data
-    return <MDXRenderer>{mdx.body}</MDXRenderer>
+    const { body, frontmatter } = mdx
+    const title = frontmatter?.title
+    return (
+        <>
+            {title && (
+                <Helmet>
+                    <title>{title}</title>
+                </Helmet>
+            )}
+            <MDXRenderer>{body}</MDXRenderer>
+        </>
+    )
 }
 
 export const pageQuery = graphql`
