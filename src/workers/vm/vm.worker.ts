8529abd256afddbfda4eab6ab638989f1217d288
Michał Moskal <michal@moskal.me>
2022-01-21 16:04:23 +0100

    build with yarn workspaces (#150)
    * build with yarn workspaces
    
    * try to get the build order right
    
    * build jacscript

--- src/workers/vm/vm.worker.ts
+++ src/workers/vm/vm.worker.ts
@@ -1,3 +1,6 @@
+import { JDBus } from "jacdac-ts"
+import { compile } from "jacscript"
+
 export interface VMMessage {
     worker: "vm"
     id?: string
@@ -8,6 +11,11 @@ export interface VMRequest extends VMMessage {
 }
 
 async function handleMessage(event: MessageEvent) {
+    if (event == null) {
+        // test imports
+        const bus = new JDBus()
+        compile(null, "")
+    }
     const message: VMRequest = event.data
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { id, worker, ...rest } = message
