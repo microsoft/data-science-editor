5fd8dd01c0643a9cd540d7465a91f7a60c1f381e
pelikhan <jhalleux@microsoft.com>
2022-01-28 14:21:18 -0800

    minify devtools page

---
+++ buildproxy.sh
@@ -0,0 +1,1 @@
+npx html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true --html5 true -o static/devtools/proxy.html static/devtools/proxy.full.html
