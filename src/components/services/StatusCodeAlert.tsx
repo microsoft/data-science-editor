59d927b237197e6b72017cbc5df32bbac214ca96
pelikhan <jhalleux@microsoft.com>
2022-02-02 14:01:35 -0800

    specialized message

--- src/components/services/StatusCodeAlert.tsx
+++ src/components/services/StatusCodeAlert.tsx
@@ -3,8 +3,18 @@ import { Alert, Typography } from "@mui/material"
 import { JDService } from "../../../jacdac-ts/src/jdom/service"
 import { RegisterOptions } from "../../jacdac/useRegisterValue"
 import useStatusCode from "./useStatusCode"
-import { SystemStatusCodes } from "../../../jacdac-ts/jacdac-spec/dist/specconstants"
+import {
+    SRV_WIFI,
+    SystemStatusCodes,
+} from "../../../jacdac-ts/jacdac-spec/dist/specconstants"
 import { humanify } from "../../../jacdac-ts/jacdac-spec/spectool/jdspec"
+
+const codes: Record<number, Record<number, (srv: JDService) => string>> = {
+    [SRV_WIFI]: {
+        [SystemStatusCodes.WaitingForInput]: () =>
+            "Waiting for WiFi credentials.",
+    },
+}
 
 export default function StatusCodeAlert(
     props: {
@@ -12,15 +22,26 @@ export default function StatusCodeAlert(
     } & RegisterOptions
 ) {
     const { service, ...rest } = props
+    const { serviceClass } = service
     const { code, vendorCode } = useStatusCode(service, rest)
     if (code === 0 && vendorCode === 0) return null
 
-    return <Alert severity="warning">
-        {!!code && <Typography variant="caption">
-            {humanify(SystemStatusCodes[code] || "?")}
-        </Typography> }
-        {!!vendorCode && <Typography variant="caption">
-            vendor error: 0x{vendorCode.toString(16)}
-        </Typography> }
-    </Alert>
+    return (
+        <Alert severity="warning">
+            {!!code && (
+                <Typography variant="caption">
+                    {humanify(
+                        codes[serviceClass]?.[code]?.(service) ||
+                            SystemStatusCodes[code] ||
+                            "?"
+                    )}
+                </Typography>
+            )}
+            {!!vendorCode && (
+                <Typography variant="caption">
+                    vendor error: 0x{vendorCode.toString(16)}
+                </Typography>
+            )}
+        </Alert>
+    )
 }
