b89ece044ab922847e3522a02bf5a33eb73eb4a8
pelikhan <jhalleux@microsoft.com>
2022-02-02 16:46:56 -0800

    starting on tree of results

--- src/pages/tools/panel-tester.tsx
+++ src/pages/tools/panel-tester.tsx
@@ -15,6 +15,9 @@ import ChipList from "../../components/ui/ChipList"
 import useDeviceImage from "../../components/devices/useDeviceImage"
 import { useDeviceSpecificationFromIdentifier } from "../../jacdac/useDeviceSpecification"
 import ImageAvatar from "../../components/tools/ImageAvatar"
+import useDevices from "../../components/hooks/useDevices"
+import { JDDevice } from "../../../jacdac-ts/src/jdom/device"
+import { isInfrastructure } from "../../../jacdac-ts/src/jdom/spec"
 
 const PANEL_MANIFEST_KEY = "panel-test-manifest"
 
@@ -109,6 +112,71 @@ A JSON formatted manifest containing an array of device specification reference 
     )
 }
 
+function PanelDeviceVerifier(props: {
+    specification: jdspec.DeviceSpec
+    device: JDDevice
+}) {
+    const { specification, device } = props
+    const { deviceId, shortId } = device
+    const count = specification.services.length
+    const services = device.services()
+    const found = services.filter(srv => !isInfrastructure(srv.specification)).length
+
+    return (
+        <tr>
+            <td>{shortId} ({deviceId})</td>
+            <td>{count}</td>
+            <td>{found}</td>
+        </tr>
+    )
+}
+
+function PanelDevicesVerifier(props: { id: string; count: number }) {
+    const { id, count } = props
+    const specification = useDeviceSpecificationFromIdentifier(id)
+    const { productIdentifiers, name } = specification
+    const devices = useDevices({
+        ignoreInfrastructure: true,
+        physical: true,
+    }).filter(d => productIdentifiers.indexOf(d.productIdentifier) > -1)
+
+    const found = devices.length
+
+    return (
+        <>
+            <tr key={id}>
+                <td>{name}</td>
+                <td>{count}</td>
+                <td>{found}</td>
+            </tr>
+            {devices?.map(device => (
+                <PanelDeviceVerifier
+                    key={device.id}
+                    specification={specification}
+                    device={device}
+                />
+            ))}
+        </>
+    )
+}
+
+function PanelVerifier(props: { panel: PanelSpec }) {
+    const { panel } = props
+    const { devices: deviceSpecs } = panel
+    return (
+        <table>
+            <tr>
+                <th>device id</th>
+                <th>count</th>
+                <th>found</th>
+            </tr>
+            {deviceSpecs.map(({ id, count }) => (
+                <PanelDevicesVerifier key={id} id={id} count={count} />
+            ))}
+        </table>
+    )
+}
+
 export default function PanelTester() {
     const [manifestSource, setManifestSource] = useLocalStorage(
         PANEL_MANIFEST_KEY,
@@ -127,6 +195,7 @@ export default function PanelTester() {
                 setSource={setManifestSource}
                 panel={panel}
             />
+            {panel && <PanelVerifier panel={panel} />}
         </Stack>
     )
 }
