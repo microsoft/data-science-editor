105ea2cb3ede318ae723a0756afe04e56639f841
James Devine <james_a_devine@outlook.com>
2022-02-23 13:29:01 +0000

    Fix vanity name if url ends with slash (#163)

--- src/components/widgets/SilkQrCode.tsx
+++ src/components/widgets/SilkQrCode.tsx
@@ -176,7 +176,7 @@ export default function SilkQRCode(props: {
 }) {
     const { url, layer, mirror = true, size = 0.3, margin = 1 } = props
     const eagleLayer = layer ?? mirror ? 21 : 22
-    const args = url.split("/")
+    const args = url.replace(/\/$/, '').split("/") // trim any trailing slash
     const vanity = args[args.length - 1]
     const { altium, kicad, scr, image, error, numBlocks } = useQRCodeSCR(
         url,
