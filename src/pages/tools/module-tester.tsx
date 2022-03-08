3929c1aba8f3224da875a43ea4b3c2f24192cce2
Peli de Halleux <pelikhan@users.noreply.github.com>
2022-03-08 21:46:46 +0000

    aria

--- src/pages/tools/module-tester.tsx
+++ src/pages/tools/module-tester.tsx
@@ -93,7 +93,7 @@ export default function Page() {
             <Grid container spacing={1}>
                 {device && <DeviceTestItem test={test} device={device} />}
             </Grid>
-            <h3>Firmwares</h3>
+            <h2>Firmwares</h2>
             <FirmwareCardGrid />
         </>
     )
