4755190a218189275d19b727c1dac12362fa9e04
pelikhan <jhalleux@microsoft.com>
2022-04-07 08:50:10 -0700

    lazier identify dialog

--- src/components/dialogs/IdentifyDialog.tsx
+++ src/components/dialogs/IdentifyDialog.tsx
@@ -1,50 +1,14 @@
-import { Dialog, DialogContent, Grid } from "@mui/material"
-import React, { useCallback, useState } from "react"
+import { Dialog, DialogContent, Grid, Typography } from "@mui/material"
+import React, { lazy, useCallback } from "react"
 import { ControlAnnounceFlags } from "../../../jacdac-ts/jacdac-spec/dist/specconstants"
 import { JDDevice } from "../../../jacdac-ts/src/jdom/device"
 import useDeviceSpecification from "../../jacdac/useDeviceSpecification"
 import DeviceName from "../devices/DeviceName"
-import useDeviceImage from "../devices/useDeviceImage"
 import useInterval from "../hooks/useInterval"
 import Alert from "../ui/Alert"
 import DialogTitleWithClose from "../ui/DialogTitleWithClose"
-
-function LazyDeviceImage(props: { device: JDDevice }) {
-    const { device } = props
-    const specification = useDeviceSpecification(device)
-    const imageUrl = useDeviceImage(specification, "lazy")
-    const largeImageUrl = useDeviceImage(specification, "catalog")
-    const [showLarge, setShowLarge] = useState(false)
-
-    if (!imageUrl) return null
-
-    const handleLargeLoaded = () => setShowLarge(true)
-
-    return (
-        <>
-            <img
-                alt="photograph of the device"
-                style={{
-                    width: "100%",
-                    display: showLarge ? undefined : "none",
-                }}
-                src={largeImageUrl}
-                onLoad={handleLargeLoaded}
-            />
-            {!showLarge && (
-                <img
-                    alt="large photograph of the device"
-                    style={{
-                        minHeight: "18rem",
-                        width: "100%",
-                        filter: "blur",
-                    }}
-                    src={imageUrl}
-                />
-            )}
-        </>
-    )
-}
+import Suspense from "../ui/Suspense"
+const LazyDeviceImage = lazy(() => import("../devices/LazyDeviceImage"))
 
 export default function IdentifyDialog(props: {
     device: JDDevice
@@ -62,6 +26,7 @@ export default function IdentifyDialog(props: {
         statusLightFlags === ControlAnnounceFlags.StatusLightRgbFade ||
         statusLightFlags === ControlAnnounceFlags.StatusLightRgbNoFade
     useInterval(open, handleSendIdentify, 5000, [device])
+    const specification = useDeviceSpecification(device)
 
     return (
         <Dialog open={open} onClose={handleCloseIdentify}>
@@ -76,8 +41,23 @@ export default function IdentifyDialog(props: {
             </DialogTitleWithClose>
             <DialogContent>
                 <Grid container alignItems="center" alignContent={"center"}>
+                    {specification && (
+                        <Grid item xs={12}>
+                            <Typography variant="caption">
+                                {[
+                                    specification.name,
+                                    specification.version,
+                                    specification.description,
+                                ]
+                                    .filter(s => !!s)
+                                    .join(", ")}
+                            </Typography>
+                        </Grid>
+                    )}
                     <Grid item xs={12}>
-                        <LazyDeviceImage device={device} />
+                        <Suspense>
+                            <LazyDeviceImage device={device} />
+                        </Suspense>
                     </Grid>
                     <Grid item xs>
                         <Alert severity="info">
