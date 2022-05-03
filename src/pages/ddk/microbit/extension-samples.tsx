7e0fec946d7ef21e1468f54259b3474f3542f37b
pelikhan <jhalleux@microsoft.com>
2022-05-02 20:12:39 -0700

    added extension

--- src/pages/ddk/microbit/extension-samples.tsx
+++ src/pages/ddk/microbit/extension-samples.tsx
@@ -25,6 +25,8 @@ export default function Samples() {
         "pelikhan/pxt-robotbit/jacdac",
 
         "pelikhan/yahboom-microbit-led-jacdac/",
+
+        "pelikhan/pxt-k-bit/jacdac",
     ]
     return (
         <>
