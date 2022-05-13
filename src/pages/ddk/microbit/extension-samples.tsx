1772abd20a3a0173e5f46cfca092ee361814570c
pelikhan <jhalleux@microsoft.com>
2022-05-13 09:26:07 -0700

    fix maqueen

--- src/pages/ddk/microbit/extension-samples.tsx
+++ src/pages/ddk/microbit/extension-samples.tsx
@@ -34,7 +34,7 @@ export default function Samples() {
         "pelikhan/pxt-k-bit/jacdac",
         "pelikhan/keystudio-relay-breakout-jacdac/",
 
-        "pxt-dfrobot_maqueenplus_v20/jacdac",
+        "pelikhan/pxt-dfrobot_maqueenplus_v20/jacdac",
     ]
     return (
         <>
