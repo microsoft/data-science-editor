33c1c6f5cc25deb7fba4fcd51d88c20cac8c21cf
pelikhan <jhalleux@microsoft.com>
2022-05-20 17:07:27 -0700

    cleanup

--- src/components/hooks/useServices.ts
+++ src/components/hooks/useServices.ts
@@ -1,11 +1,10 @@
-import { useContext } from "react"
 import { DeviceFilter } from "../../../jacdac-ts/src/jdom/filters/devicefilter"
 import { ServiceFilter } from "../../../jacdac-ts/src/jdom/filters/servicefilter"
-import JacdacContext, { JacdacContextProps } from "../../jacdac/Context"
+import useBus from "../../jacdac/useBus"
 import useChange from "../../jacdac/useChange"
 
 export default function useServices(options?: ServiceFilter & DeviceFilter) {
-    const { bus } = useContext<JacdacContextProps>(JacdacContext)
+    const bus = useBus()
     const services = useChange(bus, _ => _?.services(options) || [], [
         JSON.stringify(options),
     ])
