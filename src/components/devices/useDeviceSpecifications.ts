63cb060e2f0b9052c699b3cdba9a59d707ddb6b6
pelikhan <jhalleux@microsoft.com>
2022-05-23 17:27:19 -0700

    cleanup

--- src/components/devices/useDeviceSpecifications.ts
+++ src/components/devices/useDeviceSpecifications.ts
@@ -1,14 +1,14 @@
 import { DeviceSpecificationOptions } from "../../../jacdac-ts/src/jdom/catalog"
 import { arrayEq } from "../../../jacdac-ts/src/jdom/utils"
-import useBus from "../../jacdac/useBus"
+import useDeviceCatalog from "../../../react-jacdac/src/hooks/useDeviceCatalog"
 import useChange from "../../jacdac/useChange"
 
 export default function useDeviceSpecifications(
     options?: DeviceSpecificationOptions
 ) {
-    const bus = useBus()
+    const catalog = useDeviceCatalog()
     const specifications = useChange(
-        bus.deviceCatalog,
+        catalog,
         _ => _?.specifications(options),
         [JSON.stringify(options)],
         (a, b) => arrayEq(a, b, (l, r) => l.id === r.id)
