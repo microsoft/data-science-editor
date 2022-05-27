1f99b8de008831b75c89762169165970a27839be
pelikhan <jhalleux@microsoft.com>
2022-05-27 14:45:07 -0700

    attempt at fixing close button of video

--- src/components/youtube/YouTubeButton.tsx
+++ src/components/youtube/YouTubeButton.tsx
@@ -1,17 +1,25 @@
 import { ButtonProps } from "@mui/material"
 import { Button } from "gatsby-theme-material-ui"
 import React, { useContext } from "react"
+import useFireKey from "../hooks/useFireKey"
 import YouTubeContext from "./YouTubeContext"
 
 export default function YouTubeButton(
     props: { videoId: string } & ButtonProps
 ) {
-    const { videoId, ...rest } = props
+    const { videoId, variant = "outlined", ...rest } = props
     const { setVideoId } = useContext(YouTubeContext)
     const handleClick = () => setVideoId(videoId)
+    const fireClick = useFireKey(handleClick)
 
     return (
-        <Button {...rest} onClick={handleClick}>
+        <Button
+            variant={variant}
+            {...rest}
+            onClick={handleClick}
+            onPointerDown={handleClick}
+            onKeyDown={fireClick}
+        >
             Play Video
         </Button>
     )
