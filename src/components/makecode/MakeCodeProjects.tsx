b8b826bd3ac521b6d1a1272f99392e3b617eeadf
Tom Ball <you@example.com>
2022-05-30 15:54:23 -0700

    import ReactNode

--- src/components/makecode/MakeCodeProjects.tsx
+++ src/components/makecode/MakeCodeProjects.tsx
@@ -1,5 +1,5 @@
 import { graphql, useStaticQuery } from "gatsby"
-import React from "react"
+import ReactNode from "react"
 import { serviceSpecificationFromName } from "../../../jacdac-ts/src/jdom/spec"
 import PageLinkList from "../ui/PageLinkList"
 
