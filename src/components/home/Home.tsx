7b91459005ad4eb5c209e361e4aaaf0e400fe609
Tom Ball <you@example.com>
2022-06-03 12:06:36 -0700

    move cable, connector to home page

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -64,64 +64,8 @@ export default function Home() {
                 image={<HTML5Image />}
             />
 
-            <CenterGrid
-                subtitle="Want to know more? Read on!"
-                // imageColumns={6}
-                // image={<StaticImage src="./dashboard.png" alt="Dashboard" />}
-            />
-
-
             <SplitGrid
                 right={false}
-                subtitle="Making connections"
-                description="Jacdac's PCB edge connector is robust, double-sided, and low cost."
-                buttonText="Connector"
-                buttonVariant="link"
-                buttonUrl="/reference/connector/"
-                imageColumns={6}
-                image={
-                    <StaticImage
-                        src="./mechanicalclickconnector.png"
-                        alt="Cable and connector"
-                        imgStyle={imgStyle}
-                    />
-                }
-            />
-
-            <SplitGrid
-                right={true}
-                subtitle="Robust cables"
-                description="Cables make plug-and-play simple and error-free."
-                buttonText="Cable"
-                buttonVariant="link"
-                buttonUrl="/reference/cable/"
-                imageColumns={6}
-                image={
-                    <StaticImage
-                        src="./mechanicalclickconnector.png"
-                        alt="Cable and connector"
-                        imgStyle={imgStyle}
-                    />
-                }
-            />
-
-            <SplitGrid
-                right={false}
-                subtitle="Plays well with others"
-                description="Jacdaptors allow Jacdac to integrate with other ecosystems."
-                buttonText="Jacdaptors"
-                buttonVariant="link"
-                buttonUrl="/reference/jacdaptors"
-                imageColumns={6}
-                image={
-                    <Suspense>
-                        <JacdaptorImageList />
-                    </Suspense>
-                }
-            />
-
-            <SplitGrid
-                right={true}
                 subtitle="Services"
                 description="Jacdac services provide an abstract view of a device's features."
                 buttonText="Service catalog"
@@ -136,7 +80,7 @@ export default function Home() {
             />
 
             <SplitGrid
-                right={false}
+                right={true}
                 subtitle="Web Tools"
                 description="Visualize, debug, sniff, track, record, replay, update... from your browser"
                 buttonText="Get productive with Jacdac"
@@ -145,23 +89,105 @@ export default function Home() {
                 image={<StaticImage src="./devicetree.png" alt="Device tree" />}
             />
 
+
+            <CenterGrid
+                    title="Making Connections"
+            />
+
+
             <SplitGrid
-                right={true}
-                subtitle="Clients and Servers"
-                description="Jacdac servers (modules) encapsulate sensors/actuators. Jacdac clients (brains) are programmable."
-                buttonText="Clients and servers"
-                buttonVariant="link"
-                buttonUrl="/reference/clientserver/"
-                imageColumns={6}
+                subtitle="Cable"
+                description="Jacdac's state-of-the-art hook design delivers an impeccably fluid plug/unplug motion. Plugging the cable and experiencing a snap-fit feeling gives confidence that the connection has been made. This is achieved by two metallic hooks that snap into two slots on the board, always ensuring a robust link."
+                imageColumns={8}
                 image={
-                    <img
-                        loading="lazy"
-                        alt="jacdac bus"
-                        src={withPrefix("/images/jdbus.drawio.svg")}
-                        style={imgStyle}
+                    <StaticImage
+                        src="./fullassembly.png"
+                        alt="Close up on cable"
                     />
                 }
             />
+            
+            <SplitGrid
+                right={true}
+                subtitle="Ergonomic."
+                description=" Regardless being extremely compact (only 9.5x18.5mm), it provides an unprecedented experience in terms of comfort and ergonomic in its everyday use. This makes connecting microcontrollers and peripherals as simple as plugging a USB device into your personal computer."
+                image={<StaticImage src="./ucable.png" alt="Short cable" />}
+            />
+            
+            <SplitGrid
+                right={true}
+                subtitle="Minimum effort. Maximum flexibility."
+                description="The cable has been designed to make it extremely easy to plug in to connect modules together. While at the same time it provides a robust and reliable connection that’s difficult to unplug unvoluntarily. Bring your prototypes to the next level."
+                imageColumns={8}
+                image={
+                    <StaticImage
+                        src="./rotary.png"
+                        alt="A Jacdac rotary encoder module plugging into a Jacdac cable"
+                    />
+                }
+            />
+
+            <SplitGrid
+                subtitle="Connector"
+                description="Jacdac devices feature 3-pin PCB-based double-sided edge connectors."
+                // buttonText="Device Development Kit"
+                // buttonUrl="/ddk/"
+                imageColumns={6}
+                image={
+                    <StaticImage
+                    src="./mechanicalclickconnector.png"
+                    alt="A split view of the cable going into the PCB"
+                />
+                //<StaticImage src="./tangled.png" alt="Tangled cable" />
+                }
+            />
+
+            <SplitGrid
+                right={true}
+                subtitle="Reversible."
+                description="There's no need to worry about accidentally swapping the Power and Data wires. The PCB connector is engineered to be completely reversible, so you won't even have to think about the right orientation as you plug in the cable."
+                imageColumns={6}
+                image={
+                    <StaticImage
+                        src="./pcbfootprint.png"
+                        alt="PCB connector footprint"
+                    />
+                }
+            />
+
+            <SplitGrid
+                right={false}
+                subtitle="How does it work?"
+                description="The PCB edge connector is made of 3 gold fingers on both sides of the PCB, with two slots on the outer edges for mechanical hooking. The cable connector is made of 3 electrical contact pins and 2 mechanical hooks."
+                imageColumns={8}
+                image={
+                    <StaticImage
+                        src="./rhtemp.png"
+                        alt="A Jacdac humidity module plugging into a Jacdac cable"
+                    />
+                }
+            />
+
+
+            <SplitGrid
+                right={true}
+                subtitle="Plays well with others"
+                description="Jacdaptors allow Jacdac to integrate with other ecosystems."
+                buttonText="Jacdaptors"
+                buttonVariant="link"
+                buttonUrl="/reference/jacdaptors"
+                imageColumns={6}
+                image={
+                    <Suspense>
+                        <JacdaptorImageList />
+                    </Suspense>
+                }
+            />
+
+            <CenterGrid
+                title="Getting Technical"
+            />
+
             <SplitGrid
                 right={true}
                 subtitle="For Manufacturers"
@@ -175,6 +201,24 @@ export default function Home() {
                     <StaticImage
                         src="./pcbfootprint.png"
                         alt="PCB connector footprint"
+                    />
+                }
+            />
+
+            <SplitGrid
+                right={false}
+                subtitle="Clients and Servers"
+                description="Jacdac servers (modules) encapsulate sensors/actuators. Jacdac clients (brains) are programmable."
+                buttonText="Clients and servers"
+                buttonVariant="link"
+                buttonUrl="/reference/clientserver/"
+                imageColumns={6}
+                image={
+                    <img
+                        loading="lazy"
+                        alt="jacdac bus"
+                        src={withPrefix("/images/jdbus.drawio.svg")}
+                        style={imgStyle}
                     />
                 }
             />
