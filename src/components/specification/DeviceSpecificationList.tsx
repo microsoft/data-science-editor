8ecc40ad14dcdbecbfe4a46ee3f7b640c7493af5
pelikhan <jhalleux@microsoft.com>
2022-06-06 09:22:20 -0700

    updated sorting

--- src/components/specification/DeviceSpecificationList.tsx
+++ src/components/specification/DeviceSpecificationList.tsx
@@ -97,13 +97,13 @@ export default function DeviceSpecificationList(props: {
                 ].some(s => s?.toLowerCase()?.indexOf(query.toLowerCase()) > -1)
             )
         r = r.sort((a, b) => {
-            // use ec30 shape
-            let c = -(isEC30(a.shape) ? 1 : 0) + (isEC30(b.shape) ? 1 : 0)
-            if (c) return c
             // kits first
-            c =
+            let c =
                 -(a.tags?.indexOf("kit") > -1 ? 1 : 0) +
                 (b.tags?.indexOf("kit") > -1 ? 1 : 0)
+            if (c) return c
+            // use ec30 shape
+            c = -(isEC30(a.shape) ? 1 : 0) + (isEC30(b.shape) ? 1 : 0)
             if (c) return c
             // with connectors first
             c =
