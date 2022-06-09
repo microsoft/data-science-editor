bda6d6327a66b147cbeda0ef676c3d094aa6f838
pelikhan <jhalleux@microsoft.com>
2022-06-09 09:45:28 -0700

    attempt at using webm vifdeos

--- src/components/ui/Video.tsx
+++ src/components/ui/Video.tsx
@@ -1,25 +1,26 @@
 /* eslint-disable jsx-a11y/media-has-caption */
-import { SxProps, Theme } from "@mui/material"
 import { withPrefix } from "gatsby"
 import React, { CSSProperties, useEffect, useRef } from "react"
 import { useInView } from "react-intersection-observer"
 
 export default function Video(props: {
     mp4?: string
-    webp?: string
+    webm?: string
     poster?: string
     muted?: boolean
     loop?: boolean
     controls?: boolean
+    autoPlay?: boolean
     style?: CSSProperties
 }) {
     const {
         mp4,
-        webp,
+        webm,
         poster,
         muted = true,
         loop = true,
-        controls = true,
+        controls = false,
+        autoPlay = true,
         style,
     } = props
     const { ref, inView } = useInView()
@@ -32,24 +33,30 @@ export default function Video(props: {
             } catch (e) {
                 console.debug(e)
             }
+        } else {
+            try {
+                if (autoPlay) videoRef.current?.play()
+            } catch (e) {
+                console.debug(e)
+            }
         }
     }, [inView])
 
     return (
         <div ref={ref}>
             <video
-                style={{ width: "100%", ...(style || {}) }}
+                style={{ maxWidth: "100%", ...(style || {}) }}
                 ref={videoRef}
                 playsInline
                 controls={controls}
                 preload="auto"
-                autoPlay={inView}
+                autoPlay={autoPlay && inView}
                 muted={muted}
                 loop={loop}
                 poster={poster}
             >
                 {mp4 && <source src={withPrefix(mp4)} type="video/mp4" />}
-                {webp && <source src={withPrefix(webp)} type="video/webp" />}
+                {webm && <source src={withPrefix(webm)} type="video/webm" />}
                 Sorry, your browser does not support embedded videos.
             </video>
         </div>
