f0956e2e85ba93270a9301b6129af59ba1fe3c85
pelikhan <jhalleux@microsoft.com>
2022-06-10 07:04:37 -0700

    clean out old .gifs

--- scripts/webm-to-mp4.sh
+++ scripts/webm-to-mp4.sh
@@ -4,17 +4,17 @@ for file in $(find . -name "*.webm");
 do
     # regenerate jpg
     jpg="${file%.webm}.jpg";
-    if [[ ! -f "../../static/videos/$jpg" ]]; then
+    if [ ! -f "../../static/videos/$jpg" ]; then
         ffmpeg -y -i "$file" -frames:v 1 "../../static/videos/$jpg";
     fi
     # regenerate mp4
     mp4="${file%.webm}.mp4";
-    if [[ ! -f "../../static/videos/$mp4" ]]; then
+    if [ ! -f "../../static/videos/$mp4" ]; then
         ffmpeg -y -i "$file" -an -pix_fmt yuv420p -movflags faststart "../../static/videos/$mp4";
     fi
     # regenerate min.webm
     webm="${file%.webm}.webm";
-    if [[ ! -f "../../static/videos/$webm" ]]; then
+    if [ ! -f "../../static/videos/$webm" ]; then
         ffmpeg -y -i "$file" -an -pix_fmt yuv420p -movflags faststart "../../static/videos/$webm";
     fi
 done
