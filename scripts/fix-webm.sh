08cda0e6301b8b0324061a9b28e99c764ecb9ebe
pelikhan <jhalleux@microsoft.com>
2022-06-13 08:12:30 -0700

    normalize dashboard simulator fix size

--- scripts/fix-webm.sh
+++ scripts/fix-webm.sh
@@ -1,10 +1,13 @@
-
-cd src/videos
-for file in $(find . -name "*.webm"); 
-do
-    fixed="${file%.webm}.fixed.webm";
-    ffmpeg -y -i "$file" -an -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -preset slow -crf 18 "$fixed";
-    mv "$fixed" "$file";
-    ffmpeg -y -i "$file" -an -vf "scale='min(1080, iw)':-1" -preset slow -crf 18 "$fixed";
-    mv "$fixed" "$file";
-done
+name="$1"
+file="$name.webm"
+fixed="$name.fixed.webm";
+ffmpeg -y -i "$file" -an -vf "scale='min(1080, iw)':-1" -preset slow -crf 18 "$fixed";
+mv "$fixed" "$file";
+ffmpeg -y -i "$file" -an -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -preset slow -crf 18 "$fixed";
+mv "$fixed" "$file";
+jpg="$name.jpg";
+ffmpeg -y -i "$file" -frames:v 1 "../../static/videos/$jpg";
+mp4="$name.mp4";
+ffmpeg -y -i "$file" -pix_fmt yuv420p -movflags faststart "../../static/videos/$mp4";
+webm="$name.webm";
+ffmpeg -y -i "$file" -pix_fmt yuv420p -movflags faststart "../../static/videos/$webm";
