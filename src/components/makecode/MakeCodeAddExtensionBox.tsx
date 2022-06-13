b4f4cae387a2984f3195b6b73d086ca3ee026857
pelikhan <jhalleux@microsoft.com>
2022-06-13 14:11:36 -0700

    better deselection

--- src/components/makecode/MakeCodeAddExtensionBox.tsx
+++ src/components/makecode/MakeCodeAddExtensionBox.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import useMakeCodeEditorExtension from "./MakeCodeEditorExtensionContext"
 import DeviceSpecificationList from "../specification/DeviceSpecificationList"
+import { Button } from "@mui/material"
 
 export default function MakeCodeAddExtensionBox() {
     const { configuration, setConfiguration, device, target } =
@@ -12,16 +13,27 @@ export default function MakeCodeAddExtensionBox() {
             device: dev.id === configuration.device ? undefined : dev?.id,
         })
     }
+    const handleClear = () =>
+        setConfiguration({
+            ...configuration,
+            device: undefined,
+        })
 
     return (
-        <DeviceSpecificationList
-            makeCode={target?.id || "microbit"}
-            header={`Select your accessory (current: ${
-                device?.name || "none"
-            })`}
-            onDeviceClick={handleClick}
-            hideChips={true}
-            hideServices={true}
-        />
+        <>
+            <DeviceSpecificationList
+                makeCode={target?.id || "microbit"}
+                header={device ? `Current accessory` : `Select accessory`}
+                onDeviceClick={handleClick}
+                hideChips={true}
+                hideServices={true}
+                devices={device ? [device] : undefined}
+            />
+            {device && (
+                <Button sx={{ mt: 2 }} onClick={handleClear} variant="outlined">
+                    clear
+                </Button>
+            )}
+        </>
     )
 }
