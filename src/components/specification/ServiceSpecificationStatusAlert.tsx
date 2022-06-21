91c33e6d5272e0233efdce18125d4665cf33d784
pelikhan <jhalleux@microsoft.com>
2022-06-21 09:29:18 -0700

    better disclaimer on services

--- src/components/specification/ServiceSpecificationStatusAlert.tsx
+++ src/components/specification/ServiceSpecificationStatusAlert.tsx
@@ -1,3 +1,4 @@
+import { AlertTitle } from "@mui/material"
 import React from "react"
 import Alert from "../ui/Alert"
 
@@ -6,9 +7,20 @@ export function StatusAlert(props: { specification: jdspec.ServiceSpec }) {
 
     switch (specification?.status) {
         case "deprecated":
-            return <Alert severity="error">Deprecated</Alert>
+            return (
+                <Alert severity="error">
+                    <AlertTitle>Service deprecated</AlertTitle>
+                    This service should not be added to new modules and might
+                    not be supported in future releases.
+                </Alert>
+            )
         case "experimental":
-            return <Alert severity="info">Experimental</Alert>
+            return (
+                <Alert severity="info">
+                    <AlertTitle>Experimental service</AlertTitle>
+                    This service specification may change in the future.
+                </Alert>
+            )
         default:
             return null
     }
