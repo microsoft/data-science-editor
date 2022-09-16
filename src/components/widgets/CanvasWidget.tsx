75fe63ec48c6b3fda256bb2f01bdd6987d16c21c
Peli de Halleux <pelikhan@users.noreply.github.com>
2022-09-16 14:14:02 -0700

    Gatsby4.23 (#229)
    * upgraded deps
    
    * missing types for three
    
    * note on azure iot hub
    
    * more docs

--- src/components/widgets/CanvasWidget.tsx
+++ src/components/widgets/CanvasWidget.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unknown-property */
 import React, { useRef } from "react"
 import { Canvas, useFrame } from "@react-three/fiber"
 import { Mesh } from "three"
@@ -34,8 +35,8 @@ function Cube(props: {
 
     return (
         <mesh ref={meshRef} receiveShadow castShadow>
-            <boxBufferGeometry attach="geometry" />
-            <meshPhongMaterial attach="material" color={color} />
+            <boxGeometry />
+            <meshStandardMaterial color={color} />
         </mesh>
     )
 }
@@ -66,97 +67,6 @@ export default function CanvasWidget(props: {
                 args={[5, 5]}
                 position={[0, -1, 0]}
                 rotation={[-Math.PI / 2, 0, 0]}
-                key={""}
-                onPointerMissed={undefined}
-                material={undefined}
-                quaternion={undefined}
-                attach={undefined}
-                onUpdate={undefined}
-                up={undefined}
-                scale={undefined}
-                matrix={undefined}
-                layers={undefined}
-                dispose={undefined}
-                onClick={undefined}
-                onContextMenu={undefined}
-                onDoubleClick={undefined}
-                onPointerUp={undefined}
-                onPointerDown={undefined}
-                onPointerOver={undefined}
-                onPointerOut={undefined}
-                onPointerEnter={undefined}
-                onPointerLeave={undefined}
-                onPointerMove={undefined}
-                onPointerCancel={undefined}
-                onWheel={undefined}
-                visible={undefined}
-                type={undefined}
-                id={undefined}
-                uuid={undefined}
-                name={undefined}
-                parent={undefined}
-                modelViewMatrix={undefined}
-                normalMatrix={undefined}
-                matrixWorld={undefined}
-                matrixAutoUpdate={undefined}
-                matrixWorldNeedsUpdate={undefined}
-                frustumCulled={undefined}
-                renderOrder={undefined}
-                animations={undefined}
-                userData={undefined}
-                customDepthMaterial={undefined}
-                customDistanceMaterial={undefined}
-                isObject3D={undefined}
-                onBeforeRender={undefined}
-                onAfterRender={undefined}
-                applyMatrix4={undefined}
-                applyQuaternion={undefined}
-                setRotationFromAxisAngle={undefined}
-                setRotationFromEuler={undefined}
-                setRotationFromMatrix={undefined}
-                setRotationFromQuaternion={undefined}
-                rotateOnAxis={undefined}
-                rotateOnWorldAxis={undefined}
-                rotateX={undefined}
-                rotateY={undefined}
-                rotateZ={undefined}
-                translateOnAxis={undefined}
-                translateX={undefined}
-                translateY={undefined}
-                translateZ={undefined}
-                localToWorld={undefined}
-                worldToLocal={undefined}
-                lookAt={undefined}
-                add={undefined}
-                remove={undefined}
-                removeFromParent={undefined}
-                clear={undefined}
-                getObjectById={undefined}
-                getObjectByName={undefined}
-                getObjectByProperty={undefined}
-                getWorldPosition={undefined}
-                getWorldQuaternion={undefined}
-                getWorldScale={undefined}
-                getWorldDirection={undefined}
-                raycast={undefined}
-                traverse={undefined}
-                traverseVisible={undefined}
-                traverseAncestors={undefined}
-                updateMatrix={undefined}
-                updateMatrixWorld={undefined}
-                updateWorldMatrix={undefined}
-                toJSON={undefined}
-                clone={undefined}
-                copy={undefined}
-                addEventListener={undefined}
-                hasEventListener={undefined}
-                removeEventListener={undefined}
-                dispatchEvent={undefined}
-                geometry={undefined}
-                morphTargetInfluences={undefined}
-                morphTargetDictionary={undefined}
-                isMesh={undefined}
-                updateMorphTargets={undefined}
             />
             <Cube {...others} />
             <OrbitControls />
