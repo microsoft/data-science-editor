e45844b6c86cf7ea1b06fae169d760f52e472a72
Peli de Halleux <pelikhan@users.noreply.github.com>
2022-11-21 16:43:32 +0000

    don't load vm in jacscript-worker

--- src/workers/jacscript/jacscript-worker.ts
+++ src/workers/jacscript/jacscript-worker.ts
@@ -1,7 +1,5 @@
 import { compile, JacError, Host } from "jacscript-compiler"
 import type { DebugInfo } from "jacscript-compiler"
-import type { JacsModule } from "jacscript-vm"
-import vmMod from "jacscript-vm"
 
 export type JacscriptError = JacError
 
@@ -41,15 +39,10 @@ class WorkerHost implements Host {
     logs: string
     errors: JacError[]
 
-    constructor(
-        private specs: jdspec.ServiceSpec[],
-        private vmMod: JacsModule
-    ) {
+    constructor(private specs: jdspec.ServiceSpec[]) {
         this.files = {}
         this.logs = ""
         this.errors = []
-
-        this.vmMod.jacsInit()
 
         this.error = this.error.bind(this)
     }
@@ -66,10 +59,7 @@ class WorkerHost implements Host {
     getSpecs(): jdspec.ServiceSpec[] {
         return this.specs
     }
-    verifyBytecode(buf: Uint8Array, dbg?: DebugInfo): void {
-        const r = this.vmMod.jacsDeploy(buf)
-        if (r != 0) throw new Error("verification failed: " + r)
-    }
+    verifyBytecode(buf: Uint8Array, dbg?: DebugInfo): void {}
 }
 
 let serviceSpecs: jdspec.ServiceSpec[]
@@ -79,7 +69,7 @@ const handlers: { [index: string]: (props: any) => object | Promise<object> } =
         compile: async (props: JacscriptCompileRequest) => {
             const { source } = props
             if (!serviceSpecs) throw new Error("specs missing")
-            const host = new WorkerHost(serviceSpecs, await vmMod())
+            const host = new WorkerHost(serviceSpecs)
             const res = compile(host, source)
 
             return <Partial<JacscriptCompileResponse>>{
