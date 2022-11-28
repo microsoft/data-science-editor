277bc793989853daa7c214b9d8d37ebc20cb3d79
Peli de Halleux <pelikhan@users.noreply.github.com>
2022-11-28 23:27:48 +0000

    refactor jacscript toolbar

--- src/components/jacscript/JacscriptTextEditor.tsx
+++ src/components/jacscript/JacscriptTextEditor.tsx
@@ -1,32 +1,19 @@
 import React, { useContext, useEffect, useState, lazy } from "react"
 import { Grid, NoSsr } from "@mui/material"
 import HighlightTextField from "../ui/HighlightTextField"
-import useJacscript, { JacscriptProvider } from "./JacscriptContext"
+import useJacscript from "./JacscriptContext"
 import { useDebounce } from "use-debounce"
-import JacscriptManagerChipItems from "./JacscriptManagerChipItems"
-import useRoleManager from "../hooks/useRoleManager"
-import {
-    addServiceProvider,
-    serviceProviderDefinitionFromServiceClass,
-} from "../../../jacdac-ts/src/servers/servers"
-import RolesToolbar from "../roles/RolesToolbar"
-import {
-    LiveRoleBinding,
-    RoleBinding,
-} from "../../../jacdac-ts/src/jdom/rolemanager"
-import useBus from "../../jacdac/useBus"
 import BrainManagerToolbar from "../brains/BrainManagerToolbar"
 import BrainManagerContext from "../brains/BrainManagerContext"
 import useBrainScript from "../brains/useBrainScript"
 import useEffectAsync from "../useEffectAsync"
 import Suspense from "../ui/Suspense"
 import useJacscriptVm from "./useJacscriptVm"
+import JacscriptToolbar from "./JacscriptToolbar"
 const Dashboard = lazy(() => import("../dashboard/Dashboard"))
 
 function JacscriptTextEditorWithContext() {
     const { setSource: setJscSource, compiled } = useJacscript()
-    const bus = useBus()
-    const roleManager = useRoleManager()
     const { scriptId } = useContext(BrainManagerContext)
     const script = useBrainScript(scriptId)
     const [source, setSource] = useState("")
@@ -57,29 +44,6 @@ function JacscriptTextEditorWithContext() {
     useEffect(() => {
         setJscSource(debouncedSource)
     }, [debouncedSource])
-    // update roles
-    useEffect(() => {
-        compiled?.dbg?.roles &&
-            roleManager?.updateRoles([
-                ...compiled.dbg.roles.map(r => ({
-                    role: r.name,
-                    serviceClass: r.serviceClass,
-                })),
-            ])
-    }, [roleManager, compiled])
-
-    // start role on demand
-    const handleRoleClick = (role: RoleBinding) => {
-        const { service, preferredDeviceId, serviceClass } =
-            role as LiveRoleBinding
-        // spin off simulator
-        if (!service && !preferredDeviceId) {
-            addServiceProvider(
-                bus,
-                serviceProviderDefinitionFromServiceClass(serviceClass)
-            )
-        }
-    }
 
     const annotations = compiled?.errors?.map(
         error =>
@@ -98,12 +62,7 @@ function JacscriptTextEditorWithContext() {
                 </Grid>
             )}
             <Grid item xs={12}>
-                <RolesToolbar
-                    roleManager={roleManager}
-                    onRoleClick={handleRoleClick}
-                >
-                    <JacscriptManagerChipItems />
-                </RolesToolbar>
+                <JacscriptToolbar />
             </Grid>
             <Grid item xs={12}>
                 <HighlightTextField
--- src/components/jacscript/JacscriptToolbar.tsx
+++ src/components/jacscript/JacscriptToolbar.tsx
@@ -1,11 +1,50 @@
-import React from "react"
-import { Grid } from "@mui/material"
 import JacscriptManagerChipItems from "./JacscriptManagerChipItems"
+import RolesToolbar from "../roles/RolesToolbar"
+import React, { useEffect } from "react"
+import useJacscript from "./JacscriptContext"
+import useRoleManager from "../hooks/useRoleManager"
+import {
+    addServiceProvider,
+    serviceProviderDefinitionFromServiceClass,
+} from "../../../jacdac-ts/src/servers/servers"
+import {
+    LiveRoleBinding,
+    RoleBinding,
+} from "../../../jacdac-ts/src/jdom/rolemanager"
+import useBus from "../../jacdac/useBus"
 
 export default function JacscriptToolbar() {
+    const bus = useBus()
+    const { compiled } = useJacscript()
+    const roleManager = useRoleManager()
+
+    // update roles
+    useEffect(() => {
+        compiled?.dbg?.roles &&
+            roleManager?.updateRoles([
+                ...compiled.dbg.roles.map(r => ({
+                    role: r.name,
+                    serviceClass: r.serviceClass,
+                })),
+            ])
+    }, [roleManager, compiled])
+
+    // start role on demand
+    const handleRoleClick = (role: RoleBinding) => {
+        const { service, preferredDeviceId, serviceClass } =
+            role as LiveRoleBinding
+        // spin off simulator
+        if (!service && !preferredDeviceId) {
+            addServiceProvider(
+                bus,
+                serviceProviderDefinitionFromServiceClass(serviceClass)
+            )
+        }
+    }
+
     return (
-        <Grid container spacing={1}>
+        <RolesToolbar roleManager={roleManager} onRoleClick={handleRoleClick}>
             <JacscriptManagerChipItems />
-        </Grid>
+        </RolesToolbar>
     )
 }
