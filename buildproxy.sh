2d9b2280a1156d741b700c6b1dd559814ff49130
pelikhan <jhalleux@microsoft.com>
2023-01-05 16:17:49 -0800

    cleanup static

--- buildproxy.sh
+++
@@ -1,1 +0,0 @@
-npx html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true --html5 true -o static/devtools/proxy.html static/devtools/proxy.full.html
