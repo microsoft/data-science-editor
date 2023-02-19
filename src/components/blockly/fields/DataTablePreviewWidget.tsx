0cf3dfde4edcbbdeb94184699916d07d3e37b8e5
pelikhan <jhalleux@microsoft.com>
2023-02-19 12:48:09 -0800

    better handling of no-import data

--- src/components/blockly/fields/DataTablePreviewWidget.tsx
+++ src/components/blockly/fields/DataTablePreviewWidget.tsx
@@ -1,10 +1,15 @@
-import React from "react"
+import React, { useContext } from "react"
 import DataTableWidget from "./DataTableWidget"
 import { TABLE_PREVIEW_MAX_ITEMS } from "../toolbox"
 import { Grid } from "@mui/material"
+import useBlockData from "../useBlockData"
+import WorkspaceContext from "../WorkspaceContext"
 
 export default function DataTablePreviewWidget(props: { compare?: boolean }) {
     const { compare } = props
+    const { sourceBlock } = useContext(WorkspaceContext)
+    const { data } = useBlockData<{ id?: string } & unknown>(sourceBlock)
+
     if (!compare)
         return (
             <DataTableWidget
@@ -14,26 +19,37 @@ export default function DataTablePreviewWidget(props: { compare?: boolean }) {
                 maxItems={TABLE_PREVIEW_MAX_ITEMS}
             />
         )
-    return (
-        <Grid container spacing={1} style={{ background: "#fff" }}>
-            <Grid item xs={6}>
-                <DataTableWidget
-                    label="before"
-                    tableHeight={295}
-                    empty={"no data"}
-                    transformed={false}
-                    maxItems={TABLE_PREVIEW_MAX_ITEMS}
-                />
+    else if (!data?.length)
+        return (
+            <DataTableWidget
+                label="after"
+                tableHeight={295}
+                empty={"no data"}
+                transformed={true}
+                maxItems={TABLE_PREVIEW_MAX_ITEMS}
+            />
+        )
+    else
+        return (
+            <Grid container spacing={1} style={{ background: "#fff" }}>
+                <Grid item xs={6}>
+                    <DataTableWidget
+                        label="before"
+                        tableHeight={295}
+                        empty={"no data"}
+                        transformed={false}
+                        maxItems={TABLE_PREVIEW_MAX_ITEMS}
+                    />
+                </Grid>
+                <Grid item xs={6}>
+                    <DataTableWidget
+                        label="after"
+                        tableHeight={295}
+                        empty={"no data"}
+                        transformed={true}
+                        maxItems={TABLE_PREVIEW_MAX_ITEMS}
+                    />
+                </Grid>
             </Grid>
-            <Grid item xs={6}>
-                <DataTableWidget
-                    label="after"
-                    tableHeight={295}
-                    empty={"no data"}
-                    transformed={true}
-                    maxItems={TABLE_PREVIEW_MAX_ITEMS}
-                />
-            </Grid>
-        </Grid>
-    )
+        )
 }
